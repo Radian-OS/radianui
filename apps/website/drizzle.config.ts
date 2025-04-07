99fd2d3b6071f05de144c6911e980e7a79809c21
BeEnAyA <106618310+BeEnAyA@users.noreply.github.com>
2025-04-07 11:43:57 +0545

    fix: db generate and db migrate

--- apps/website/drizzle.config.ts
+++ apps/website/drizzle.config.ts
@@ -8,5 +8,5 @@ export default defineConfig({
   dbCredentials: {
     url: process.env.DATABASE_URL!,
     ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
-  },
+  }
 } satisfies Config)
