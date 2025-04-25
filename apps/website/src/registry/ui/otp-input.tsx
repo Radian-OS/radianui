72ea7672989c4f31af6f4cdcbdad7f6499e7b53e
BeEnAyA <106618310+BeEnAyA@users.noreply.github.com>
2025-04-25 14:02:18 +0545

    fix: lint errors before push

--- apps/website/src/registry/ui/otp-input.tsx
+++ apps/website/src/registry/ui/otp-input.tsx
@@ -1,58 +1,65 @@
-"use client"
+// "use client"
 
-import * as React from "react"
-import { OTPInput as Root,OTPInputContext,REGEXP_ONLY_CHARS,REGEXP_ONLY_DIGITS_AND_CHARS,REGEXP_ONLY_DIGITS,type OTPInputProps,type SlotProps } from "input-otp"
-import { cva } from "class-variance-authority"
-import { cn } from "@/lib/utils"
+// import * as React from "react"
+// import { cva } from "class-variance-authority"
+// import { type OTPInputProps, type SlotProps } from "input-otp"
+// import { cn } from "@/lib/utils"
 
-type OTPInput = React.ComponentProps<typeof Root> & {
-	length?: number
-	size?: "28" | "32" | "36" | "40" | "44" | "48" | "56"
-	placeholder?: string
-	label?: string
-	className?: string
-}
+// type OTPInput = Pick<
+// 	OTPInputProps,
+// 	| "value"
+// 	| "onChange"
+// 	| "containerClassName"
+// 	| "onComplete"
+// 	| "placeholder"
+// 	| "textAlign"
+// 	| "inputMode"
+// 	| "pattern"
+// 	| "pasteTransformer"
+// 	| "pushPasswordManagerStrategy"
+// 	| "noScriptCSSFallback"
+// 	| "className"
+// 	| "disabled"
+// > & {
+// 	length?: number
+// 	size?: "28" | "32" | "36" | "40" | "44" | "48" | "56"
+// 	label?: string
+// }
 
-const slotVariants = cva(
-	"relative rounded-lg shadow-2xs bg-bg-base text-text flex items-center justify-center placeholder:select-none appearance-none transition-all disabled:cursor-not-allowed focus:outline-none focus:ring-2 focus:ring-primary-focus focus:border-primary border border-border-alpha",
-	{
-		variants: {
-			size: {
-				"28": "size-7 text-xs",
-				"32": "size-8 text-sm",
-				"36": "size-9 text-sm",
-				"40": "size-10 text-sm",
-				"44": "size-11 text-sm",
-				"48": "size-12 text-base",
-				"56": "size-14 text-base",
-			},
-		},
-	}
-)
+// const slotVariants = cva(
+// 	"relative rounded-lg shadow-2xs bg-bg-base text-text flex items-center justify-center placeholder:select-none appearance-none transition-all disabled:cursor-not-allowed focus:outline-none focus:ring-2 focus:ring-primary-focus focus:border-primary border border-border-alpha",
+// 	{
+// 		variants: {
+// 			size: {
+// 				"28": "size-7 text-xs",
+// 				"32": "size-8 text-sm",
+// 				"36": "size-9 text-sm",
+// 				"40": "size-10 text-sm",
+// 				"44": "size-11 text-sm",
+// 				"48": "size-12 text-base",
+// 				"56": "size-14 text-base",
+// 			},
+// 		},
+// 	}
+// )
 
-function OTPInput(){
+// function OTPInput(props: OTPInput) {}
 
-}
+// function Slot(props: SlotProps) {
+// 	return (
+// 		<div className={cn()}>
+// 			{props.char !== null && <div>{props.char}</div>}
+// 			{props.hasFakeCaret && <FakeCaret />}
+// 		</div>
+// 	)
+// }
 
+// function FakeCaret() {
+// 	return (
+// 		<div className="animate-caret-blink pointer-events-none absolute inset-0 flex items-center justify-center">
+// 			<div className="h-8 w-px bg-white" />
+// 		</div>
+// 	)
+// }
 
-function Slot(props: SlotProps) {
-	return (
-	  <div
-		className={cn()}
-	  >
-		{props.char !== null && <div>{props.char}</div>}
-		{props.hasFakeCaret && <FakeCaret />}
-	  </div>
-	)
-  }
-
-
-  function FakeCaret() {
-	return (
-	  <div className="absolute pointer-events-none inset-0 flex items-center justify-center animate-caret-blink">
-		<div className="w-px h-8 bg-white" />
-	  </div>
-	)
-  }
-
-export { OTPInput }
+// export { OTPInput }
