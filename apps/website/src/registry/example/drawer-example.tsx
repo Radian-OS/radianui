915c70296aa37c674a13ba9b411d900685f59b89
ashmit <ashmitbastola@gmail.com>
2025-04-29 13:39:19 +0545

    refactor:drawer and alert update

--- apps/website/src/registry/example/drawer-example.tsx
+++ apps/website/src/registry/example/drawer-example.tsx
@@ -54,11 +54,11 @@ export default function DrawerExample() {
 					handle={handle}
 					backdrop={backdrop}
 					trigger={<Button>Open Drawer</Button>}
-					// rounded={rounded}
+				// rounded={rounded}
 				>
 					<DrawerHeader>
-						<span className="text-lg font-semibold">Component Sample</span>
-						<span className="text-text-secondary text-sm">Stress testing the drawer component with an example</span>
+						<div className="text-lg font-semibold">Component Sample</div>
+						<div className="text-text-secondary text-sm">Stress testing the drawer component with an example</div>
 					</DrawerHeader>
 
 					<Tabs defaultValue="invoice">
@@ -70,60 +70,64 @@ export default function DrawerExample() {
 						</DrawerBody>
 						<DrawerFooter>
 							<TabsContent className="flex flex-col gap-4" value="invoice">
-								<div className="bg-bg-level1 flex gap-4 rounded-[0.5rem] p-4">
-									<Avatar name="avatar" size={"48"} variant="circle" src="/avatar.png" />
-									<div>
-										<h1 className="text-base font-semibold">Anna Mureum</h1>
-										<p className="text-text-secondary text-sm">Head of Engineering</p>
+								<div className="flex flex-col" >
+									<div className="bg-bg-level1 flex gap-4 rounded-radius-md py-4">
+										<Avatar name="avatar" size={"48"} variant="circle" src="/avatar.png" />
+										<div>
+											<h1 className="text-base font-semibold">Anna Mureum</h1>
+											<p className="text-text-secondary text-sm">Head of Engineering</p>
+										</div>
 									</div>
+									<div>
+										<ul className="flex flex-col gap-3">
+											<li className="flex gap-2">
+												<span className="text-text-tertiary w-1/2 text-sm">Status</span>
+												<span className="w-1/2">
+													<Badge className="bg-success border-none text-white" size="20">
+														Paid
+													</Badge>
+												</span>
+											</li>
+											<li className="flex gap-2">
+												<span className="text-text-tertiary w-1/2 text-sm">Customer ID</span>
+												<span className="w-1/2 text-sm">1200322201A</span>
+											</li>
+											<li className="flex gap-2">
+												<span className="text-text-tertiary w-1/2 text-sm">Invoice ID</span>
+												<span className="w-1/2 text-sm">44440000AY</span>
+											</li>
+											<li className="flex gap-2">
+												<span className="text-text-tertiary w-1/2 text-sm">Payment Method</span>
+												<span className="w-1/2 text-sm">Credit card ending with 0044</span>
+											</li>
+											<li className="flex gap-2">
+												<span className="text-text-tertiary w-1/2 text-sm">Due Date</span>
+												<span className="w-1/2 text-sm">Jan 12, 2024</span>
+											</li>
+										</ul>
+									</div>
+									<div>
+										<Divider orientation="horizontal" spacing="8" />
+									</div>
+									<div className="text-sm font-semibold">More details about the invoice</div>
+									<div className="text-text-tertiary bg-fill-level2 flex items-center justify-center rounded-[0.75rem] p-5 mt-2 text-sm">Sample Container</div>
 								</div>
-								<div>
-									<ul className="flex flex-col gap-3">
-										<li className="flex gap-2">
-											<span className="text-text-tertiary w-1/2 text-sm">Status</span>
-											<span className="w-1/2">
-												<Badge className="bg-success border-none text-white" size="20">
-													Paid
-												</Badge>
-											</span>
-										</li>
-										<li className="flex gap-2">
-											<span className="text-text-tertiary w-1/2 text-sm">Customer ID</span>
-											<span className="w-1/2 text-sm">1200322201A</span>
-										</li>
-										<li className="flex gap-2">
-											<span className="text-text-tertiary w-1/2 text-sm">Invoice ID</span>
-											<span className="w-1/2 text-sm">44440000AY</span>
-										</li>
-										<li className="flex gap-2">
-											<span className="text-text-tertiary w-1/2 text-sm">Payment Method</span>
-											<span className="w-1/2 text-sm">Credit card ending with 0044</span>
-										</li>
-										<li className="flex gap-2">
-											<span className="text-text-tertiary w-1/2 text-sm">Due Date</span>
-											<span className="w-1/2 text-sm">Jan 12, 2024</span>
-										</li>
-									</ul>
-								</div>
-								<div>
-									<Divider orientation="horizontal" spacing="8" />
-								</div>
-								<div className="text-sm font-semibold">More details about the invoice</div>
-								<div className="text-text-tertiary bg-bg-level1 flex items-center justify-center rounded-[0.75rem] p-5 text-sm">Sample Container</div>
 							</TabsContent>
-							<TabsContent value="updates" className="flex flex-col items-center justify-center gap-8">
-								<div className="h-[108px] w-[163px]">
-									<Image src="/loader.png" className="h-full w-full object-cover" width={100} height={100} alt="loader" />
-								</div>
-								<div className="flex flex-col gap-1 text-center">
-									<h1 className="text-lg font-semibold">No new updates</h1>
-									<span className="text-text-secondary text-sm">This content sample does not have any new updates, please check at a later time</span>
-								</div>
-								<div className="flex gap-3">
-									<DrawerClose>
-										<Button variant="neutral-outline">Close Drawer</Button>
-									</DrawerClose>
-									<Button>Refresh</Button>
+							<TabsContent value="updates">
+								<div className="py-4 flex flex-col gap-3 items-center" >
+									<div className="h-[108px] w-[163px]">
+										<Image src="/loader.png" className="h-full w-full object-cover" width={100} height={100} alt="loader" />
+									</div>
+									<div className="flex flex-col gap-1 text-center">
+										<h1 className="text-lg font-semibold">No new updates</h1>
+										<span className="text-text-secondary text-sm">This content sample does not have any new updates, please check at a later time</span>
+									</div>
+									<div className="flex gap-3">
+										<DrawerClose>
+											<Button variant="neutral-outline">Close Drawer</Button>
+										</DrawerClose>
+										<Button>Refresh</Button>
+									</div>
 								</div>
 							</TabsContent>
 						</DrawerFooter>
