db11416dac1c448028a7defff209a2802662ddb9
yube01 <yubraja008@gmail.com>
2025-05-01 13:59:23 +0545

    Fix:Date picker

--- apps/website/src/component-preview/date-picker-preview.tsx
+++ apps/website/src/component-preview/date-picker-preview.tsx
@@ -12,6 +12,7 @@ import {
 } from "@/registry/ui/dropdown"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/registry/ui/tabs"
 import DatePicker from "@/registry/ui/date-picker"
+import { Button } from "@/registry/ui/button"
 
 
 export type SizeOptions = "28" | "32" | "36" | "40" | "44" | "48"
@@ -29,6 +30,7 @@ const DatePickerPreview = () => {
     const [showDateRangeShortcut, setShowDateRangeShortcut] = useState<boolean>(false)
     const [doubleCalendar, setDoubleCalendar] = useState<boolean>(false)
     const [showTime, setShowTime] = useState<boolean>(false)
+    const [footer, setFooter] = useState<boolean>(false)
 
 
     return (
@@ -140,6 +142,19 @@ const DatePickerPreview = () => {
                                     </DropdownGroup>
                                 </DropdownSubContent>
                             </DropdownSub>
+                            <DropdownSub>
+                                <DropdownSubTrigger>Footer</DropdownSubTrigger>
+                                <DropdownSubContent>
+                                    <DropdownGroup
+                                        selectionMode="single"
+                                        selectedValues={[String(footer)]}
+                                        onSelectedChange={(values) => setFooter(values[0] === "true")}
+                                        minSelectionCount={1}>
+                                        <DropdownItem value="true">Yes</DropdownItem>
+                                        <DropdownItem value="false">No</DropdownItem>
+                                    </DropdownGroup>
+                                </DropdownSubContent>
+                            </DropdownSub>
                         </DropdownContent>
                     </Dropdown>
                 </div>
@@ -160,6 +175,10 @@ const DatePickerPreview = () => {
                         showTime={showTime}
                         size={size}
                         rounded={rounded}
+                        footer={footer && (<div className="p-3 flex gap-2 w-full justify-end">
+                            <Button variant="neutral-outline">Cancel</Button>
+                            <Button>Apply</Button>
+                        </div>)}
                     />
                 </div>
             </TabsContent>
