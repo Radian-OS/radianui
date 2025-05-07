e933f340bde4b3c2483f3f35fa2b6b13ddea1f95
BeEnAyA <106618310+BeEnAyA@users.noreply.github.com>
2025-05-07 09:00:34 +0545

    commit before pull and merge

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from "@/components/theme-provider"
 import Banner from "@/components/under-development-banner"
 import "@/css/globals.css"
 import { cn } from "@/lib/utils"
+import { generatePalette, PaletteScheme } from "@/lib/rainbow/palette-generator"
+import { HSLtoHEX } from "@/lib/rainbow/color-converter"
 
 export const metadata: Metadata = {
 	title: "Radian",
@@ -16,9 +18,26 @@ export default async function RootLayout({
 }: Readonly<{
 	children: React.ReactNode
 }>) {
+	const hex= "#8170ff"
+	const palette= generatePalette(hex,PaletteScheme.TRIADIC)
+	console.log(JSON.stringify(palette, null, 2))
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<body className={cn("relative min-h-svh", "antialiased")}>
+				{palette.colors.map((color,idx)=>(
+					<div key={idx} className="relative size-20 rounded border shadow-sm" style={{ backgroundColor: HSLtoHEX(color.shades[0].hue, color.shades[0].saturation, color.shades[0].luminosity) }}>
+						<div
+							className={cn(
+								"absolute inset-0 flex items-center justify-center rounded text-sm font-medium",
+								Number(color.shades[0].luminosity) < 500 ? "text-black" : "text-white"
+							)}>
+							<div className="flex flex-col items-center justify-center">
+								<span>{palette.name}</span>
+								<span>{HSLtoHEX(color.shades[0].hue, color.shades[0].saturation, color.shades[0].luminosity)}</span>
+							</div>
+						</div>
+					</div>
+				))}
 				<PostHogProvider>
 					<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
 						<header className="sticky top-0 z-50 w-full">
