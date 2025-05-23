3313d262d725783ed2f7c4cc32038074b9f8797c
yube01 <yubraja008@gmail.com>
2025-05-23 12:56:30 +0545

    Time picker primitive added

---
+++ apps/website/src/component-preview/time-picker-preview.tsx
@@ -0,0 +1,173 @@
+import { useState } from "react"
+import { CodeArea } from "@/registry/ui/code"
+import {
+	Dropdown,
+	DropdownContent,
+	DropdownGroup,
+	DropdownItem,
+	DropdownSub,
+	DropdownSubContent,
+	DropdownSubTrigger,
+	DropdownTrigger,
+} from "@/registry/ui/dropdown"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/registry/ui/tabs"
+import TimePicker from "@/registry/ui/time-picker"
+
+export type SizeOptions = "28" | "32" | "36" | "40" | "44" | "48"
+export type RoundedOptions = "xs" | "sm" | "md" | "lg" | "xl" | "2xl"
+const roundedOptions = ["xs", "sm", "md", "lg", "xl", "2xl"]
+const sizes = ["28", "32", "36", "40", "44", "48"]
+
+const TimePickerPreview = () => {
+	const [rounded, setRounded] = useState<RoundedOptions>("lg")
+	const [size, setSize] = useState<SizeOptions>("36")
+	const [disabled, setDisabled] = useState<boolean>(false)
+	const [label, setLabel] = useState<boolean>(true)
+
+	const [is24Hour, setIs24Hour] = useState<boolean>(false)
+	const [interval, setInterval] = useState<number>(15)
+
+	const intervalOptions = ["15", "30", "60"]
+
+	return (
+		<Tabs defaultValue="preview" className="mb-10">
+			<div className="flex items-center justify-between">
+				<div className="flex items-center gap-3">
+					<Dropdown>
+						<DropdownTrigger>Properties</DropdownTrigger>
+						<DropdownContent>
+							<DropdownSub>
+								<DropdownSubTrigger>Rounded</DropdownSubTrigger>
+								<DropdownSubContent>
+									<DropdownGroup
+										selectionMode="single"
+										selectedValues={[rounded]}
+										onSelectedChange={(values) => setRounded(values[0] as RoundedOptions)}
+										minSelectionCount={1}>
+										{roundedOptions.map((roundedOption) => (
+											<DropdownItem value={roundedOption} key={roundedOption}>
+												{roundedOption}
+											</DropdownItem>
+										))}
+									</DropdownGroup>
+								</DropdownSubContent>
+							</DropdownSub>
+							<DropdownSub>
+								<DropdownSubTrigger>Size</DropdownSubTrigger>
+								<DropdownSubContent>
+									<DropdownGroup
+										selectionMode="single"
+										selectedValues={[size]}
+										onSelectedChange={(values) => setSize(values[0] as SizeOptions)}
+										minSelectionCount={1}>
+										{sizes.map((size) => (
+											<DropdownItem value={size} key={size}>
+												{size}
+											</DropdownItem>
+										))}
+									</DropdownGroup>
+								</DropdownSubContent>
+							</DropdownSub>
+							<DropdownSub>
+								<DropdownSubTrigger>Label</DropdownSubTrigger>
+								<DropdownSubContent>
+									<DropdownGroup
+										selectionMode="single"
+										selectedValues={[String(label)]}
+										onSelectedChange={(values) => setLabel(values[0] === "true")}
+										minSelectionCount={1}>
+										<DropdownItem value="true">True</DropdownItem>
+										<DropdownItem value="false">False</DropdownItem>
+									</DropdownGroup>
+								</DropdownSubContent>
+							</DropdownSub>
+							<DropdownSub>
+								<DropdownSubTrigger>Disabled</DropdownSubTrigger>
+								<DropdownSubContent>
+									<DropdownGroup
+										selectionMode="single"
+										selectedValues={[String(disabled)]}
+										onSelectedChange={(values) => setDisabled(values[0] === "true")}
+										minSelectionCount={1}>
+										<DropdownItem value="true">True</DropdownItem>
+										<DropdownItem value="false">False</DropdownItem>
+									</DropdownGroup>
+								</DropdownSubContent>
+							</DropdownSub>
+
+							<DropdownSub>
+								<DropdownSubTrigger>24 Hour Mode</DropdownSubTrigger>
+								<DropdownSubContent>
+									<DropdownGroup
+										selectionMode="single"
+										selectedValues={[String(is24Hour)]}
+										onSelectedChange={(values) => setIs24Hour(values[0] === "true")}
+										minSelectionCount={1}>
+										<DropdownItem value="true">True</DropdownItem>
+										<DropdownItem value="false">False</DropdownItem>
+									</DropdownGroup>
+								</DropdownSubContent>
+							</DropdownSub>
+
+							<DropdownSub>
+								<DropdownSubTrigger>Interval</DropdownSubTrigger>
+								<DropdownSubContent>
+									<DropdownGroup
+										selectionMode="single"
+										selectedValues={[String(interval)]}
+										onSelectedChange={(values) => setInterval(Number(values[0]))}
+										minSelectionCount={1}>
+										{intervalOptions.map((option) => (
+											<DropdownItem key={option} value={option}>
+												{`${option} min`}
+											</DropdownItem>
+										))}
+									</DropdownGroup>
+								</DropdownSubContent>
+							</DropdownSub>
+						</DropdownContent>
+					</Dropdown>
+				</div>
+				<TabsList>
+					<TabsTrigger value="preview">Preview</TabsTrigger>
+					<TabsTrigger value="code">Code</TabsTrigger>
+				</TabsList>
+			</div>
+
+			<TabsContent value="preview">
+				<div className="flex h-[420px] items-center justify-center overflow-auto rounded-xl border px-10">
+					<TimePicker
+						label={label ? "Time Picker" : undefined}
+						rounded={rounded as RoundedOptions}
+						size={size as SizeOptions}
+						disabled={disabled}
+						is24Hour={is24Hour}
+						interval={interval}
+						placeholder="Select time"
+						classNames={{ base: "w-[320px] flex" }}
+					/>
+				</div>
+			</TabsContent>
+
+			<TabsContent value="code">
+				<CodeArea
+					language="tsx"
+					showLineNumbers
+					className="h-[420px]"
+					code={`<TimePicker
+    label="${label}"
+    rounded="${rounded}"
+    size={${size}}
+    disabled={${disabled}}
+    is24Hour={${is24Hour}}
+    interval={${interval}}
+    placeholder="Select time"
+    classNames={{ base: "w-[320px] flex" }}
+/>`}
+				/>
+			</TabsContent>
+		</Tabs>
+	)
+}
+
+export default TimePickerPreview
