9f92705fd9fffe3879a4337f7020e51fce14817a
BeEnAyA <106618310+BeEnAyA@users.noreply.github.com>
2025-06-03 13:47:49 +0545

    component: skeleton ongoing

---
+++ apps/website/src/registry/ui/skeleton.tsx
@@ -0,0 +1,7 @@
+import { cn } from "@/lib/utils"
+
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+	return <div data-slot="skeleton" className={cn("bg-fill-level3 animate-pulse rounded-md", className)} {...props} />
+}
+
+export { Skeleton }
