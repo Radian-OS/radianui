ad22db3f2412a9d159c3e69320cfd7cd7615566e
Bijay Khapung <khapungbj84@gmail.com>
2025-06-04 10:38:32 +0545

    changes in blocks
    - move blocks from /test to /blocks
    - load preview's device same as user device
    - maintain 16/9 aspect ratio in hero page

---
+++ apps/website/src/app/(app)/blocks/page.tsx
@@ -0,0 +1,14 @@
+import { blocks } from "@/data/blocks"
+import BlockPreview from "./block-viewer"
+
+const page = () => {
+	return (
+		<div>
+			{blocks.map((block, index) => (
+				<BlockPreview preview={block.preview} code={block.code} title={block.title} category={block.category} key={index} />
+			))}
+		</div>
+	)
+}
+
+export default page
