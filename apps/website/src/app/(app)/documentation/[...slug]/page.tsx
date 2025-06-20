3f6e00c3af8b02c2e9727029def480b5ac9afa86
Be-En-AyA <bt.binaya@gmail.com>
2025-06-20 09:31:42 +0545

    fix(metadata): remove the robots field metadata

--- apps/website/src/app/(app)/documentation/[...slug]/page.tsx
+++ apps/website/src/app/(app)/documentation/[...slug]/page.tsx
@@ -27,39 +27,6 @@ export async function generateStaticParams() {
 	}))
 }
 
-// ✅ Await `params` inside generateMetadata
-// export async function generateMetadata({ params }: { params: Promise<{ slug: string[] }> }): Promise<Metadata> {
-// 	const resolvedParams = await params
-// 	const slug = resolvedParams.slug.join("/")
-// 	const doc = allDocs.find((d) => d.slugAsParams === slug)
-
-// 	if (!doc) {
-// 		return {
-// 			title: "Documentation Not Found - Radian",
-// 			description: "The page you're looking for doesn't exist. Browse our docs to learn more about Radian UI components.",
-// 		}
-// 	}
-
-// 	const url = `https://radianos.com/documentation/${slug}`
-// 	const cleanTitle = `${doc.title} - Radian`
-// 	const fullDesc = `${doc.description}`
-
-// 	return {
-// 		title: cleanTitle,
-// 		description: fullDesc,
-// 		keywords: doc.keywords ?? ["Radian", "React components", "Tailwind CSS", "UI library", "design system", "developer tools"],
-// 		openGraph: {
-// 			title: cleanTitle,
-// 			description: fullDesc,
-// 			url,
-// 		},
-// 		twitter: {
-// 			card: "summary_large_image",
-// 			title: cleanTitle,
-// 			description: fullDesc,
-// 		},
-// 	}
-// }
 export async function generateMetadata({ params }: { params: Promise<{ slug: string[] }> }): Promise<Metadata> {
 	const resolvedParams = await params
 	const slug = resolvedParams.slug.join("/")
--- apps/website/src/app/(app)/layout.tsx
+++ apps/website/src/app/(app)/layout.tsx
@@ -1,29 +1,5 @@
 import React, { ReactNode } from "react"
-import { Metadata } from "next"
 import Navbar from "@/components/navbar"
-
-export const metadata: Metadata = {
-	title: `${"Radian"} - Ship next generation of world class products and solutions`,
-	description: "Welcome to Radian—your go-to React & Tailwind CSS lib.",
-	openGraph: {
-		title: "Radian Landing Page",
-		description: "Discover Radian, the ultimate React & Tailwind component lib.",
-		url: `${process.env.NEXT_PUBLIC_WEBSITE_URL}`,
-		images: [
-			{
-				url: `${process.env.NEXT_PUBLIC_WEBSITE_URL}/og/static-og.png`,
-				width: 1200,
-				height: 630,
-			},
-		],
-	},
-	twitter: {
-		card: "summary_large_image",
-		title: "Radian Landing Page",
-		description: "Discover Radian, the ultimate React & Tailwind component lib.",
-		images: [`${process.env.NEXT_PUBLIC_WEBSITE_URL}/og/static-og.png`],
-	},
-}
 
 interface Props {
 	children: ReactNode
