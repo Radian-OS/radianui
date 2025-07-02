d37b58c6e4b4dbb4efab6a5a9970d40cfd2e203d
Ashmit72 <ashmitbastola@gmail.com>
2025-07-02 09:32:33 +0545

    test(button):fancy button test

--- apps/website/src/component-preview/fancy-button-preview.tsx
+++ apps/website/src/component-preview/fancy-button-preview.tsx
@@ -121,6 +121,19 @@ const FancyButtonPreview = () => {
 							Fancy Button 2
 						</Button>
 					)}
+
+					<Button
+						size={size}
+						disabled={disabled === "true"}
+						className="border-primary-stroke mt-2 border bg-gradient-to-b from-[#6347EB] to-[#5133CF] shadow-[0px_4px_4px_rgba(24,25,27,0.16),0px_0px_0px_1.5px_#5B3FE0] hover:from-[#6A52F2] hover:to-[#5B3FE0]">
+						Fancy Button
+					</Button>
+					<Button
+						size={size}
+						disabled={disabled === "true"}
+						className="border-primary-stroke mt-2 border bg-gradient-to-b from-[#5133CF] to-[#6347EB] shadow-[0px_4px_4px_rgba(24,25,27,0.16),0px_0px_0px_1.5px_#5B3FE0] hover:from-[#5B3FE0] hover:to-[#6A52F2]">
+						Fancy Button 2
+					</Button>
 				</div>
 			</TabsContent>
 			<TabsContent value="code">
