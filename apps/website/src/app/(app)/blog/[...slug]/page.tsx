12c1dde15192981333d4490f027052ff66040ac9
Ashmit72 <ashmitbastola@gmail.com>
2025-07-07 11:01:03 +0545

    update(mdx):added blog alongside mdx

--- apps/website/src/app/(app)/blog/[...slug]/page.tsx
+++ apps/website/src/app/(app)/blog/[...slug]/page.tsx
@@ -1,7 +1,32 @@
 import React from "react"
+import { allBlogs } from "contentlayer/generated"
+import { notFound } from "next/navigation"
 import { Badge } from "@/registry/ui/badge"
 
-const page = () => {
+interface BlogPageProps {
+	params: Promise<{ slug: string[] }> // 👈 Promise-based!
+}
+
+async function getBlogFromParams({ params }: BlogPageProps) {
+	const resolvedParams = await params
+	const slug = resolvedParams.slug.join("/") || ""
+	const blog = allBlogs.find((blog) => blog.slugAsParams === slug)
+	return blog ?? null
+}
+
+export async function generateStaticParams() {
+	return allBlogs.map((blog) => ({
+		slug: blog.slugAsParams.split("/"),
+	}))
+}
+
+export default async function BlogPage({ params }: BlogPageProps) {
+	// const resolvedParams = await params
+	const blog = await getBlogFromParams({ params })
+	// const currentPath = `/docs/${resolvedParams.slug.join("/")}`
+	// const category = blog?.slugAsParams.split("/")[0]
+	if (!blog) return notFound()
+
 	return (
 		<div className="max-w-360 mb-70.5 mx-auto mt-20 flex w-full flex-col items-center">
 			<div className="max-w-200 w-full">
@@ -14,5 +39,3 @@ const page = () => {
 		</div>
 	)
 }
-
-export default page
