140d810a1db2a620f9351c00d3d2f043ecb23cda
Ashmit72 <ashmitbastola@gmail.com>
2025-07-11 13:23:28 +0545

    update(blog):layout fix

--- apps/website/src/app/(app)/blog/layout.tsx
+++ apps/website/src/app/(app)/blog/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-	return <div className="mb-43 max-w-200 mx-auto mt-10 px-5">{children}</div>
+	return <div className="mb-43 max-w-200 mx-auto mt-10 px-5 lg:px-0">{children}</div>
 }
 
 export default layout
