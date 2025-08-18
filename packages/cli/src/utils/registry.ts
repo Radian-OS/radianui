6f16ebbf88006046dccb8a68b8beec2ae781b061
Be-En-AyA <bt.binaya@gmail.com>
2025-08-18 11:12:19 +0545

    fix(cli): updated the registry.ts file

--- packages/cli/src/utils/registry.ts
+++ packages/cli/src/utils/registry.ts
@@ -177,8 +177,6 @@ export const getFont = async (font: Font): Promise<FontData> => {
 	}
 }
 
-
-
 /**
  * Resolves components and their registry dependencies recursively from the registry.
  * @param registryComponents
