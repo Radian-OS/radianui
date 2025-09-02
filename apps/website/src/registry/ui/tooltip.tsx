049e2154572a1f67d6860113a440782b06d4d9f2
Be-En-AyA <bt.binaya@gmail.com>
2025-09-02 13:57:44 +0545

    fix: tooltip initial height

--- apps/website/src/registry/ui/tooltip.tsx
+++ apps/website/src/registry/ui/tooltip.tsx
@@ -45,7 +45,7 @@ function TooltipContent({ className, sideOffset = 6, children, ...props }: Toolt
 			side={side}
 			sideOffset={sideOffset}
 			className={cn(
-				"outline-hidden animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 relative z-50 rounded-md bg-black px-2 py-1 text-center text-xs text-white shadow-md dark:bg-white dark:text-black",
+				"outline-hidden animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 relative z-50 rounded-md bg-black px-2 py-1 text-center text-xs/tight text-white shadow-md dark:bg-white dark:text-black",
 				className
 			)}
 			{...props}>
