b382586d68fe3b6141d308dd09d5eaef84344f1d
Ashmit72 <ashmitbastola@gmail.com>
2025-09-18 13:33:27 +0545

    fix: overlay alert dialog

--- apps/website/src/registry/ui/alert-dialog.tsx
+++ apps/website/src/registry/ui/alert-dialog.tsx
@@ -19,7 +19,7 @@ type AlertDialogActionProps = React.ComponentProps<typeof AlertDialogPrimitive.A
 type AlertDialogCancelProps = React.ComponentProps<typeof AlertDialogPrimitive.Cancel>
 
 const alertDialogOverlayVariants = cva(
-	"data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50",
+	"data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-[999]",
 	{
 		variants: {
 			backdrop: {
@@ -61,7 +61,7 @@ function AlertDialogContent({ className, backdrop, ...props }: AlertDialogConten
 			<AlertDialogPrimitive.Content
 				data-slot="alert-dialog-content"
 				className={cn(
-					"bg-bg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 border-alpha fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-5 shadow-lg shadow-black/5 duration-200 sm:rounded-lg",
+					"bg-bg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 border-alpha fixed left-[50%] top-[50%] z-[999] grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-5 shadow-lg shadow-black/5 duration-200 sm:rounded-lg",
 					className
 				)}
 				{...props}
