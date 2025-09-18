d64ebf336e0c112a2f1f8c10b6b4eb333ad09094
Ashmit72 <ashmitbastola@gmail.com>
2025-09-18 11:17:24 +0545

    fix: theme optimization and card resoponsiveness

--- apps/website/src/components/common/cookie-card.tsx
+++ apps/website/src/components/common/cookie-card.tsx
@@ -4,25 +4,32 @@ import { Switch } from "@/registry/ui/switch"
 
 const CookieCard = () => {
 	return (
-		<div className="bg-bg border-border flex h-full w-full flex-col justify-between rounded-2xl border p-5">
-			<span>
+		<div className="bg-bg border-border flex h-full w-full flex-col justify-between gap-4 rounded-2xl border p-5">
+			{/* Title */}
+			<div>
 				<p className="text-fg text-base font-medium">Cookie Settings</p>
 				<p className="text-fg-secondary text-sm font-normal">Manage your cookie settings here.</p>
-			</span>
-			<span className="flex items-center justify-between">
-				<span>
+			</div>
+
+			{/* Strictly Necessary */}
+			<div className="flex flex-wrap items-center justify-between gap-2">
+				<div className="min-w-[200px]">
 					<p className="text-fg text-base font-medium">Strictly Necessary</p>
 					<p className="text-fg-secondary text-sm font-normal">These cookies are necessary for website functionality.</p>
-				</span>
+				</div>
 				<Switch defaultChecked size="24" />
-			</span>
-			<span className="flex items-center justify-between">
-				<span>
+			</div>
+
+			{/* Functional Cookies */}
+			<div className="flex flex-wrap items-center justify-between gap-2">
+				<div className="min-w-[200px]">
 					<p className="text-fg text-base font-medium">Functional Cookies</p>
 					<p className="text-fg-secondary text-sm font-normal">These cookies enable personalized features on the site.</p>
-				</span>
+				</div>
 				<Switch defaultChecked size="24" />
-			</span>
+			</div>
+
+			{/* Button */}
 			<Button className="w-full" variant="outline" color="primary">
 				Save preferences
 			</Button>
