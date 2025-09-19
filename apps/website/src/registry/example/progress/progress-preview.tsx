fe8e140a09893583b0c102554377dea48544e0f8
yube01 <yubraja008@gmail.com>
2025-09-19 12:18:44 +0545

    fix: build fix

--- apps/website/src/registry/example/progress/progress-preview.tsx
+++ apps/website/src/registry/example/progress/progress-preview.tsx
@@ -1,7 +1,7 @@
 import { ProgressBar } from "@/registry/ui/progress-bar"
 
-const ProgressBarPreview = () => {
+const ProgressPreview = () => {
 	return <ProgressBar className="w-80" value={50} />
 }
 
-export default ProgressBarPreview
+export default ProgressPreview
