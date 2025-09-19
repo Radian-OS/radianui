73c0f36747f5db0e6ad89e867e2186fe37bef163
yube01 <yubraja008@gmail.com>
2025-09-19 14:03:25 +0545

    chore: progress example added

--- apps/website/src/registry/example/progress/progress-preview.tsx
+++ apps/website/src/registry/example/progress/progress-preview.tsx
@@ -1,7 +1,7 @@
-import { ProgressBar } from "@/registry/ui/progress-bar"
+import { Progress } from "@/registry/ui/progress-bar"
 
 const ProgressPreview = () => {
-	return <ProgressBar className="w-80" value={50} />
+	return <Progress className="w-80" value={50} />
 }
 
 export default ProgressPreview
