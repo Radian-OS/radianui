cf24f8ca71a08f80403ee89440ecd856ebef3922
Be-En-AyA <bt.binaya@gmail.com>
2025-09-22 07:22:06 +0545

    refactor: label component

--- apps/website/src/registry/ui/label.tsx
+++ apps/website/src/registry/ui/label.tsx
@@ -4,7 +4,9 @@ import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cn } from "@/lib/utils"
 
-function Label({ className, ...props }: React.ComponentProps<typeof LabelPrimitive.Root>) {
+export type LabelProps = React.ComponentProps<typeof LabelPrimitive.Root>
+
+function Label({ className, ...props }: LabelProps) {
 	return <LabelPrimitive.Root className={cn("peer-disabled:text-fg-tertiary text-sm font-medium peer-disabled:cursor-not-allowed", className)} {...props} />
 }
 Label.displayName = LabelPrimitive.Root.displayName
