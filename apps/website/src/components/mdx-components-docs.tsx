5da28501ef8a6673af3b9598de5a2a5d74c6ca0f
Ashmit72 <ashmitbastola@gmail.com>
2025-09-24 10:40:43 +0545

    fix: update examples for button

--- apps/website/src/components/mdx-components-docs.tsx
+++ apps/website/src/components/mdx-components-docs.tsx
@@ -23,7 +23,6 @@ import PhoneNumberPreview from "@/component-preview/phone-number-preview"
 import SearchPreview from "@/component-preview/search-preview"
 import SocialButtonPreview from "@/component-preview/social-button-preview"
 import SpinnerPreview from "@/component-preview/spinner-preview"
-import SvgButtonPreview from "@/component-preview/svg-button-preview"
 import SwitchPreview from "@/component-preview/switch-preview"
 import TextAreaPreview from "@/component-preview/text-area-preview"
 import Installation from "@/components/installation"
@@ -135,7 +134,6 @@ const components = (examples: typeof Examples | undefined) => ({
 	FancyButtonPreview: () => <FancyButtonPreview />,
 	CompactButtonPreview: () => <CompactButtonPreview />,
 	ContentBasedTextAreaExample: () => <ContentBasedTextAreaExample />,
-	SvgButtonPreview: () => <SvgButtonPreview />,
 	CodeWithTabsPreview: () => <CodeWithTabs />,
 	CodeWithCopyExample: () => <CodeWithCopyExample />,
 
