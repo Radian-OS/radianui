01cf0d813d2e93f75f9323e72a817f59f5582b22
yube01 <yubraja008@gmail.com>
2025-09-24 13:52:40 +0545

    chore: file upload example added

--- apps/website/src/registry/example/file-upload/file-upload-example.tsx
+++ apps/website/src/registry/example/file-upload/file-upload-example.tsx
@@ -1,7 +1,13 @@
 import { Input } from "@/registry/ui/input"
+import { Label } from "@/registry/ui/label"
 
 const FileUploadExample = () => {
-	return <Input className="w-80" type="file" />
+	return (
+		<div className="flex flex-col gap-1.5">
+			<Label>File Upload</Label>
+			<Input className="w-80" type="file" />
+		</div>
+	)
 }
 
 export default FileUploadExample
