b23cff6f408909ce6fe97c6de1829213c39e4bc4
Bijay Khapung <khapungbj84@gmail.com>
2025-09-25 12:10:49 +0545

    feat: add switch and spinner example

---
+++ apps/website/src/registry/example/spinner/spinner-preview.tsx
@@ -0,0 +1,12 @@
+import { Spinner } from "@/registry/ui/spinner"
+
+export default function SpinnerPreview() {
+	return (
+		<div className="flex gap-6">
+			<Spinner variant="activity" />
+			<Spinner variant="default" />
+			<Spinner variant="simple" />
+			<Spinner variant="wave" />
+		</div>
+	)
+}
