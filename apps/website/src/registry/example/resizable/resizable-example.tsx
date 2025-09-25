9a4b73b3732e228c870ce6f0210c88934a0ccf30
yube01 <yubraja008@gmail.com>
2025-09-25 12:19:51 +0545

    chore: resizable example updated

--- apps/website/src/registry/example/resizable/resizable-example.tsx
+++ apps/website/src/registry/example/resizable/resizable-example.tsx
@@ -3,16 +3,16 @@ import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/registry
 const ResizableExample = () => {
 	return (
 		<div className="mx-auto max-h-[200px] max-w-3xl">
-			<ResizablePanelGroup direction="horizontal" className="my-10 min-h-[200px] max-w-md rounded-lg border md:min-w-[450px]">
+			<ResizablePanelGroup direction="horizontal" className="my-10 min-h-[200px] max-w-md rounded-lg border md:min-w-[350px]">
 				<ResizablePanel defaultSize={50}>
-					<ResizablePanelGroup direction="vertical">
+					<ResizablePanelGroup direction="horizontal">
 						<ResizablePanel defaultSize={25}>
 							<div className="flex h-full items-center justify-center border-b p-6">
-								<span className="font-semibold">Navbar</span>
+								<span className="font-semibold">Sidebar</span>
 							</div>
 						</ResizablePanel>
 						<ResizableHandle withHandle />
-						<ResizablePanel defaultSize={75}>
+						<ResizablePanel defaultSize={50}>
 							<div className="flex h-full items-center justify-center p-6">
 								<span className="font-semibold">Body</span>
 							</div>
