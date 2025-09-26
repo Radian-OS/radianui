44bb2fffb3fb41097cbbe2eedfa165a34fa9a72a
Bijay Khapung <khapungbj84@gmail.com>
2025-09-26 13:25:32 +0545

    feat: add combobox

--- apps/website/src/config/navigation-config.ts
+++ apps/website/src/config/navigation-config.ts
@@ -167,6 +167,14 @@ export const navigationItems: NavigationSection[] = [
 				alt: "Collapsible UI component illustration",
 			},
 			{
+				title: "Combobox",
+				description: "Combobox Component",
+				url: "/docs/components/combobox",
+				thumbnail: "/thumbnails/coming-soon.webp",
+				thumbnailDark: `/thumbnails/coming-soon-dark.webp`,
+				alt: "Collapsible UI component illustration",
+			},
+			{
 				title: "Command",
 				description: "Command Component",
 				url: "/docs/components/command",
---
+++ apps/website/src/registry/example/combobox/combobox-preview.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import * as React from "react"
+import { Check, ChevronsUpDown } from "lucide-react"
+import { cn } from "@/lib/utils"
+import { Button } from "@/registry/ui/button"
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/registry/ui/command"
+import { Popover, PopoverContent, PopoverTrigger } from "@/registry/ui/popover"
+
+const topCities = [
+	{
+		value: "amsterdam",
+		label: "Amsterdam, Netherlands",
+	},
+	{
+		value: "london",
+		label: "London, UK",
+	},
+	{
+		value: "paris",
+		label: "Paris, France",
+	},
+	{
+		value: "tokyo",
+		label: "Tokyo, Japan",
+	},
+	{
+		value: "new_york",
+		label: "New York, USA",
+	},
+	{
+		value: "dubai",
+		label: "Dubai, UAE",
+	},
+]
+
+export default function ComboboxDemo() {
+	const [open, setOpen] = React.useState(false)
+	const [value, setValue] = React.useState("")
+
+	return (
+		<Popover open={open} onOpenChange={setOpen}>
+			<PopoverTrigger asChild>
+				<Button variant="outline" color="neutral" role="combobox" aria-expanded={open} className="w-[200px] justify-between">
+					<span className="truncate">{value ? topCities.find((city) => city.value === value)?.label : "Select city..."}</span>
+					<ChevronsUpDown />
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent className="w-(--radix-popper-anchor-width) p-0">
+				<Command className="border-0">
+					<CommandInput placeholder="Search city..." />
+					<CommandList>
+						<CommandEmpty>No city found.</CommandEmpty>
+						<CommandGroup>
+							{topCities.map((city) => (
+								<CommandItem
+									key={city.value}
+									value={city.value}
+									onSelect={(currentValue) => {
+										setValue(currentValue === value ? "" : currentValue)
+										setOpen(false)
+									}}>
+									<span className="truncate">{city.label}</span>
+									<Check className={cn("ml-auto", value === city.value ? "opacity-100" : "opacity-0")} />
+								</CommandItem>
+							))}
+						</CommandGroup>
+					</CommandList>
+				</Command>
+			</PopoverContent>
+		</Popover>
+	)
+}
