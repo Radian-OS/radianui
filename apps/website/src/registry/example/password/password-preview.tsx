2fc467b8c7707f14de6f9f5eefc282b41af03a11
Be-En-AyA <bt.binaya@gmail.com>
2025-10-10 10:44:38 +0545

    chore: onging password examples using input component

---
+++ apps/website/src/registry/example/password/password-preview.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Input } from "@/registry/ui/input"
+import { Label } from "@/registry/ui/label"
+
+export default function PasswordPreview() {
+	return (
+		<div className="flex flex-col gap-1.5">
+			<Label htmlFor="password">Password</Label>
+			<Input id="password" className="w-80" type="password" placeholder="Enter your password" />
+		</div>
+	)
+}
