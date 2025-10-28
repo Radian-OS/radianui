1b51e47ce60763f59e3050887f0b21ba589cd872
Ashmit72 <ashmitbastola@gmail.com>
2025-10-28 11:55:32 +0545

    fix: landing page features section update

--- apps/website/src/components/home/features-section.tsx
+++ apps/website/src/components/home/features-section.tsx
@@ -1,5 +1,34 @@
-import { Component } from "lucide-react"
+"use client"
+
+import {
+	AppWindow,
+	Calendar as CalendarIcon,
+	ChartLine,
+	CircleGauge,
+	CircleUser,
+	ClipboardCheck,
+	Component,
+	FolderGit,
+	LayoutDashboard,
+	Loader,
+	MousePointerClick,
+	PanelRight,
+	ScanEye,
+	Siren,
+	SlidersHorizontal,
+	SquareArrowDown,
+	SquareCheck,
+	SquareCode,
+	SwatchBook,
+	Table,
+	Tag,
+	Terminal,
+	TextCursorInput,
+	ToggleLeft,
+} from "lucide-react"
 import { Badge } from "@/registry/ui/badge"
+import { Button } from "@/registry/ui/button"
+import { Calendar } from "@/registry/ui/calendar"
 
 export default function FeaturesSection() {
 	return (
@@ -7,7 +36,7 @@ export default function FeaturesSection() {
 			<div className="flex flex-col items-center gap-8">
 				<div className="relative">
 					<Badge className="relative z-10" variant="soft" color="neutral" size="28">
-						<Component className="text-primary" /> Features
+						<Component className="text-primary" /> Rapid Development
 					</Badge>
 
 					<svg className="absolute bottom-1/2 left-1/2 -translate-x-1/2" width="1095" height="350" viewBox="0 0 1095 350" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -59,12 +88,139 @@ export default function FeaturesSection() {
 			<div className="border-border w-full max-w-[1440px] rounded-xl border">
 				<div className="border-border flex h-[584px] border-b">
 					<div className="flex-1/3 border-border border-r">
-						<div className="flex flex-col gap-4 p-5">
-							<h6 className="heading-6">Copy-paste or install via NPM</h6>
-							<p className="text-fg-secondary">Use instantly by copying the code or installing via CLI — no setup friction, just build.</p>
+						<div className="flex flex-col gap-4 pl-16 pt-16">
+							<Terminal className="bg-primary-focus stroke-primary border-primary rounded-xs cursor-pointer border-2" />
+							<h6 className="heading-6">Copy-paste or install via command line</h6>
+							<p className="text-fg-secondary">Install with one command or copy the snippet. No configuration. No waiting. Just build.</p>
 						</div>
 					</div>
-					<div className="flex-1"></div>
+					<div className="flex-2/3 flex flex-col gap-4 pl-16 pt-16">
+						<ScanEye className="text-primary" size={28} />
+						<h6 className="heading-6">Accessible Components</h6>
+						<p className="text-fg-secondary max-w-116 w-full">From keyboard navigation to structural semantics, everything follows modern accessibility standards.</p>
+					</div>
+				</div>
+
+				<div className="border-border flex h-[568px] border-b">
+					<div className="flex-1/3 pb-23.25 relative flex flex-col justify-between pl-16 pt-16">
+						<div className="flex flex-col gap-4">
+							<ClipboardCheck size={28} className="text-primary" />
+							<h6 className="heading-6">Clean, Modular Components</h6>
+							<p className="text-fg-secondary">Radian’s building blocks are composable, easy to override, and perfect for scaling projects.</p>
+						</div>
+						<div className="flex flex-wrap items-center gap-3">
+							<Badge size="28" variant="outline" color="neutral">
+								<SquareArrowDown />
+								Accordion
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<PanelRight />
+								Drawers
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<Tag />
+								Drawers
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<MousePointerClick />
+								Button
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<Siren />
+								Alert
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<CircleUser />
+								Avatar
+							</Badge>
+							<Badge size="28">
+								<CalendarIcon />
+								Calendar
+							</Badge>
+
+							<Badge size="28" variant="outline" color="neutral">
+								<SlidersHorizontal />
+								Slider
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<SquareCheck />
+								Checkbox
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<AppWindow />
+								Dialogs
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<Loader />
+								Loader
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<SquareCode />
+								Code
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<Table />
+								Table
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<ChartLine />
+								Chart
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<ToggleLeft />
+								Switch
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<TextCursorInput />
+								Input Feilds
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<Component />
+								Toaster
+							</Badge>
+							<Badge size="28" variant="outline" color="neutral">
+								<Component />
+								Banner
+							</Badge>
+						</div>
+					</div>
+					<div className="flex-2/3 flex flex-col items-center justify-center gap-4 pl-16 pt-16">
+						<div className="border-alpha bg-elevation-level1 rounded-xl border">
+							<Calendar className="border-0 bg-transparent pb-2" mode="range" numberOfMonths={2} showOutsideDays={false} />
+							<div className="border-border flex justify-end gap-2 border-t px-2 py-3">
+								<Button variant="outline" color="neutral">
+									Cancel
+								</Button>
+								<Button>Apply</Button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="border-border flex h-[584px] border-b">
+					<div className="flex-2/3 border-border flex flex-col gap-4 border-r pl-16 pt-16">
+						<LayoutDashboard className="text-primary" size={28} />
+						<h6 className="heading-6">Reusable UI Blocks</h6>
+						<p className="text-fg-secondary max-w-116 w-full">From keyboard navigation to structural semantics, everything follows modern accessibility standards.</p>
+					</div>
+					<div className="flex-1/3">
+						<div className="pr-15.75 flex flex-col gap-4 pl-16 pt-16">
+							<CircleGauge className="text-primary" size={28} />
+							<h6 className="heading-6">Performance First Design</h6>
+							<p className="text-fg-secondary">Built with lightweight code and minimal DOM to deliver fast-loading, snappy experiences.</p>
+						</div>
+					</div>
+				</div>
+				<div className="border-border flex h-[584px] border-b-0">
+					<div className="border-border flex w-1/2 flex-col gap-4 border-r pl-16 pt-16">
+						<FolderGit className="text-primary" size={28} />
+						<h6 className="heading-6">Seamless Design-to-Code Sync</h6>
+						<p className="text-fg-secondary max-w-116 w-full">Changes made in Figma are easily replicable in the code, guaranteeing pixel-perfect consistency.</p>
+					</div>
+					<div className="flex w-1/2 flex-col gap-4 pl-16 pt-16">
+						<SwatchBook className="text-primary" size={28} />
+						<h6 className="heading-6">Streamlined component library</h6>
+						<p className="text-fg-secondary max-w-116 w-full">Our vast collection of customizable components eliminates the need to reinvent the wheel.</p>
+					</div>
 				</div>
 			</div>
 		</div>
