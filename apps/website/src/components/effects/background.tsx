8f38c028ede595ba1d91d32a7783f4dcc8af1a0e
Bijay Khapung <khapungbj84@gmail.com>
2025-10-30 09:09:05 +0545

    chore: update docs

--- apps/website/src/components/effects/background.tsx
+++ apps/website/src/components/effects/background.tsx
@@ -10,6 +10,7 @@ export default function Background({ children }: { children?: React.ReactNode })
 			{/* Neutral radial background overlay */}
 			<div className="bg-radial-[at_50%_100%] from-bg/12 to-bg absolute top-[380px] z-20 aspect-[2/1] max-h-[960px] w-full rounded-t-full to-75%" />
 
+			{/* Noise */}
 			<div className="pointer-events-none absolute inset-0 bg-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSIjMDAwIi8+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSJ1cmwoI25vaXNlKSIvPjxmaWx0ZXIgaWQ9Im5vaXNlIj48ZmVUdXJidWxlbmNlIHR5cGU9ImZyYWN0YWxOb2lzZSIgYmFzZUZyZXF1ZW5jeT0iMC44IiBudW1PY3RhdmVzPSIxMCIvPjwvZmlsdGVyPjxyZWN0IHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCIgZmlsdGVyPSJ1cmwoI25vaXNlKSIgZmlsbD0iIzAwMCIgb3BhY2l0eT0iMC4wNSIvPjwvc3ZnPg==')] bg-[length:64px_64px] bg-repeat opacity-10 mix-blend-overlay" />
 
 			{/* Half circle flickering grid */}
--- apps/website/src/components/home/features-section.tsx
+++ apps/website/src/components/home/features-section.tsx
@@ -90,7 +90,13 @@ export default function FeaturesSection() {
 						<Component className="text-primary" /> Rapid Development
 					</Badge>
 
-					<svg className="absolute bottom-1/2 left-1/2 -translate-x-1/2" width="1095" height="350" viewBox="0 0 1095 350" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<svg
+						className="not-md:hidden absolute bottom-1/2 left-1/2 -translate-x-1/2"
+						width="1095"
+						height="350"
+						viewBox="0 0 1095 350"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg">
 						<path
 							d="M0.433594 1.2504L70.1484 122C73.1494 127.198 78.6956 130.4 84.6976 130.4L177.71 130.4C184.379 130.4 190.417 134.346 193.095 140.454L280.203 339.097C282.882 345.205 288.886 349.15 295.555 349.15C444.741 349.15 649.277 349.15 798.467 349.15C805.147 349.15 811.159 345.193 813.831 339.071L900.956 139.479C903.629 133.358 909.674 129.4 916.353 129.4L1009.35 129.4C1015.35 129.4 1020.89 126.198 1023.89 121L1093.61 0.250397"
 							strokeWidth="1"
