568b1625bde8c65a9c13948ae40c9aa17c5adea4
Bijay Khapung <khapungbj84@gmail.com>
2025-10-30 13:08:50 +0545

    refactor(switch): change text color to fg

--- apps/website/src/registry/example/switch/square-switch.tsx
+++ apps/website/src/registry/example/switch/square-switch.tsx
@@ -8,9 +8,7 @@ export default function SquareSwitch() {
 	return (
 		<div className="flex items-center space-x-2">
 			<Switch id={id} shape="square" />
-			<Label htmlFor={id} className="text-fg-secondary">
-				Turn on notifications
-			</Label>
+			<Label htmlFor={id}>Turn on notifications</Label>
 		</div>
 	)
 }
