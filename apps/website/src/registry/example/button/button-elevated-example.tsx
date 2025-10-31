b91caad5258c84973dfd9f874e0e26a210b4d696
Ashmit72 <ashmitbastola@gmail.com>
2025-10-31 10:34:13 +0545

    fix: button elevated example add

---
+++ apps/website/src/registry/example/button/button-elevated-example.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { Button } from "@/registry/ui/button"
+
+const ButtonElevatedExample = () => {
+	return (
+		<div className="w-50 flex flex-col items-center justify-center gap-3">
+			<Button className="active:bg-primary w-full shadow-[0_2px_4px_0_rgba(35,20,85,0.16),0_-2px_0_0_rgba(0,0,0,0.3)_inset] hover:shadow-[0_2px_4px_0_rgba(35,20,85,0.12),0_-2px_0_0_rgba(0,0,0,0.2)_inset] active:shadow-[0_10px_12px_0_rgba(35,19,85,0.12),0_2px_4px_0_rgba(35,20,85,0.12)]">
+				Elevated Button
+			</Button>
+			<Button
+				className="active:bg-elevation-level1 w-full shadow-[0_-2px_0_0_var(--color-border)_inset,0_1px_1px_0_rgba(25,24,27,0.04)] active:shadow-none"
+				color="neutral"
+				variant="outline">
+				Elevated Neutral
+			</Button>
+		</div>
+	)
+}
+
+export default ButtonElevatedExample
