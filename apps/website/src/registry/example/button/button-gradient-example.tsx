bb0253661d407a826c5599c01bece99616846771
Ashmit72 <ashmitbastola@gmail.com>
2025-10-31 09:12:38 +0545

    fix: add gradient button

--- apps/website/src/registry/example/button/button-gradient-example.tsx
+++ apps/website/src/registry/example/button/button-gradient-example.tsx
@@ -4,11 +4,13 @@ import { Button } from "@/registry/ui/button"
 const ButtonGradientExample = () => {
 	return (
 		<div className="w-50 flex flex-col items-center justify-center gap-3">
-			<Button className="relative w-full overflow-hidden border border-black/10">
+			<Button className="relative w-full overflow-hidden border border-black/10 before:pointer-events-none before:absolute before:inset-0 before:bg-gradient-to-b before:from-white/20 before:to-white/0">
 				Gradient
-				<span className="pointer-events-none absolute inset-0 z-0 size-full bg-gradient-to-b from-white/20 to-white/0" />
 			</Button>
-			<Button className="to-white/16 relative w-full overflow-hidden bg-gradient-to-b from-white">Gradient w/shadow</Button>
+
+			<Button className="before:to-white/16 relative w-full overflow-hidden shadow-[0_0_0_1px_#683BFF,0_1px_2px_0_rgba(27,11,104,0.32),0_6px_16px_0_rgba(27,11,104,0.32)] before:pointer-events-none before:absolute before:inset-0 before:bg-gradient-to-b before:from-white/0">
+				Gradient w/shadow
+			</Button>
 		</div>
 	)
 }
