3e660957381c2a3380a37d00d145902801c032b2
Ashmit72 <ashmitbastola@gmail.com>
2025-10-31 11:48:14 +0545

    fix: buttons as input example update

--- apps/website/src/registry/example/button/button-input-example.tsx
+++ apps/website/src/registry/example/button/button-input-example.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { format } from "date-fns"
 import { Calendar as CalendarIcon, ChevronDown } from "lucide-react"
 import { Button } from "@/registry/ui/button"
 import { Calendar } from "@/registry/ui/calendar"
@@ -8,30 +9,43 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/registry/ui/popover"
 const ButtonExampleInput = () => {
 	const [open, setOpen] = React.useState(false)
 	const [date, setDate] = React.useState<Date>()
+	const [value, setValue] = React.useState("Last 30 Days")
+
+	const options = ["Last 7 Days", "Last 30 Days", "Last 90 Days", "Last 6 Months", "Last Year"]
+
 	return (
 		<div className="flex flex-wrap items-center justify-center gap-3">
 			<Dropdown>
 				<DropdownTrigger asChild>
 					<Button color="neutral" variant="outline">
 						<CalendarIcon />
-						Last 30 Days <ChevronDown />
+						{value}
+						<ChevronDown />
 					</Button>
 				</DropdownTrigger>
 				<DropdownContent className="w-39.5">
-					<DropdownItem>English (Default)</DropdownItem>
-					<DropdownItem>Spanish</DropdownItem>
-					<DropdownItem>French</DropdownItem>
-					<DropdownItem>Chinese</DropdownItem>
-					<DropdownItem>German</DropdownItem>
+					{options.map((option) => (
+						<DropdownItem key={option} onSelect={() => setValue(option)}>
+							{option}
+						</DropdownItem>
+					))}
 				</DropdownContent>
 			</Dropdown>
 
 			<Popover open={open} onOpenChange={setOpen}>
 				<PopoverTrigger asChild>
-					<Button color="neutral" variant="outline">
-						<CalendarIcon />
-						Pick Date Range
-					</Button>
+					<div className="relative w-[250px]">
+						<Button id="date" type="button" variant="outline" color="neutral">
+							{date ? (
+								format(date, "PPP")
+							) : (
+								<>
+									<CalendarIcon />
+									<span>Pick Date Range</span>
+								</>
+							)}
+						</Button>
+					</div>
 				</PopoverTrigger>
 				<PopoverContent className="w-auto p-0" align="start">
 					<Calendar
