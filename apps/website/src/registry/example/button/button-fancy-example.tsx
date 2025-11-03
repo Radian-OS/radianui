99af08c47369a807452c5e78091ee3d1e49b79c8
Ashmit72 <ashmitbastola@gmail.com>
2025-11-03 12:11:48 +0545

    chore: commit changes

--- apps/website/src/registry/example/button/button-fancy-example.tsx
+++ apps/website/src/registry/example/button/button-fancy-example.tsx
@@ -3,14 +3,11 @@ import { Button } from "@/registry/ui/button"
 
 const ButtonFancyExample = () => {
 	return (
-		<div className="w-50 flex flex-col items-center justify-center gap-3">
-			{/* variant1 */}
-			<Button className="relative w-full overflow-hidden rounded-lg bg-gradient-to-b from-[#542AC7] to-[#6F38FF] shadow-[0_4px_4px_0_rgba(24,25,27,0.16),0_0_0_1.5px_#683BFF] ring-1 ring-white/10 hover:shadow-[0_6px_8px_0_rgba(24,25,27,0.12),0_0_0_1.5px_#683BFF] hover:brightness-105">
+		<div className="w-50 flex flex-col items-center justify-center gap-4">
+			<Button className="from-primary to-primary-hover before:border-white/16 hover:opacity-92 w-full overflow-hidden bg-gradient-to-b before:absolute before:inset-px before:rounded-lg before:border">
 				Top Dark
 			</Button>
-
-			{/* variant2 */}
-			<Button className="relative w-full overflow-hidden rounded-lg bg-gradient-to-b from-[#6F38FF] to-[#542AC7] shadow-[0_4px_4px_0_rgba(24,25,27,0.16),0_0_0_1.5px_#683BFF] ring-1 ring-white/10 hover:shadow-[0_6px_8px_0_rgba(24,25,27,0.12),0_0_0_1.5px_#683BFF] hover:brightness-105">
+			<Button className="from-primary-hover to-primary before:border-white/16 hover:opacity-92 w-full overflow-hidden bg-gradient-to-b before:absolute before:inset-px before:rounded-lg before:border">
 				Top Shine
 			</Button>
 		</div>
