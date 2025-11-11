854f030a62de409f455960a24bed1868bbf4a030
Ashmit72 <ashmitbastola@gmail.com>
2025-11-11 10:57:19 +0545

    chore: homepage update

--- apps/website/src/app/(app)/page.tsx
+++ apps/website/src/app/(app)/page.tsx
@@ -1,41 +1,163 @@
-import React from "react"
-import Footer from "@/components/footer"
-import A from "@/components/home/A"
-import B from "@/components/home/B"
-import EmailSection from "@/components/home/email-section"
-import FundamentalSection from "@/components/home/fundamental-section"
-import HeroSection from "@/components/home/hero-section"
-import UiComponentsSection from "@/components/home/ui-components-section"
-import { cn } from "@/lib/utils"
+import { Box, Code, SearchCode } from "lucide-react"
+import Link from "next/link"
+import Background from "@/components/effects/background"
+import ComponentsSection from "@/components/home/components-section"
+import CTASection from "@/components/home/cta-section"
+import FAQSection from "@/components/home/faq-section"
+import FeaturesSection from "@/components/home/features-section"
+import FooterSection from "@/components/home/footer-section"
+import HomeInteractive from "@/components/home/home-interactive"
+import InvertedSection from "@/components/home/inverted-section"
+import PlaygroundSection from "@/components/home/playground-section"
+import VideoSection from "@/components/home/video-section"
+import { BorderBeam } from "@/registry/animated/border-beam"
+import { Badge } from "@/registry/ui/badge"
+import { Button } from "@/registry/ui/button"
 
-export default function Home() {
+export default function Page() {
 	return (
-		<div className="w-full overflow-x-hidden">
-			<div className="border-alpha max-w-310 mx-auto box-border border-l border-r">
-				<div className="relative">
-					<div className="relative overflow-hidden pb-20 pt-12">
-						<HeroSection />
-						<div className="group hidden xl:block">
-							<A
-								className={cn(
-									"animate-float right-25 -skew-x-14 skew-y-5 duration-2500 group-hover:skew-y-4 group-hover:animation-pause absolute top-10 scale-90 group-hover:-skew-x-6 group-hover:duration-700"
-								)}
-							/>
-							<B
-								className={cn(
-									"animate-float -skew-x-14 skew-y-5 duration-2500 group-hover:skew-y-4 group-hover:animation-pause absolute -right-5 top-32 scale-90 group-hover:-skew-x-6 group-hover:duration-700"
-								)}
-							/>
+		<div className="min-h-screen w-full overflow-x-hidden">
+			<Background>
+				<div className="pt-30 flex flex-col items-center justify-center gap-12">
+					<div className="max-w-250 flex flex-col items-center justify-center gap-6">
+						<div className="relative h-[28px] rounded-md">
+							<Badge size="28">
+								<Box size={16} />
+								Under Development - Alpha Release
+							</Badge>
+							<BorderBeam size={50} />
 						</div>
+						<h1 className="heading-1 from-fg to-fg-secondary bg-gradient-to-b bg-clip-text text-center text-transparent">Build next gen of world class products and solutions</h1>
+						<p className="text-fg-secondary text-center text-lg font-normal">
+							Radian is a high-quality, flexible and open-source, design and development library built using React and Tailwind. Start your next product here
+						</p>
 					</div>
-					{/* For gradient background */}
-					<div className="from-hero-fade-gradient-from to-hero-fade-gradient-to -ml-396 bg-linear-to-b md:h-25 lg:h-50 absolute bottom-0 w-[calc(100%+999rem)]"></div>
+					<div className="flex w-full flex-col items-center justify-center gap-4 sm:flex-row">
+						<Link href="/docs/components/accordion" className="w-full sm:w-fit">
+							<Button size="36" className="w-full sm:w-fit" variant="outline" color="neutral">
+								<SearchCode />
+								Browse Components
+							</Button>
+						</Link>
+						<Button
+							size="36"
+							className="border-primary-hover w-full border bg-gradient-to-b from-[#6347EB] to-[#5133CF] shadow-[0px_4px_4px_rgba(24,25,27,0.16)] ring-[1.5px] ring-[#5B3FE0] hover:from-[#6A52F2] hover:to-[#5B3FE0] sm:w-fit">
+							<Code />
+							Copy Terminal Command
+						</Button>
+					</div>
 				</div>
-				<FundamentalSection />
-				<UiComponentsSection />
-				<EmailSection />
-			</div>
-			<Footer />
+
+				<div className="mt-27 relative mx-auto h-[860px] max-w-[1400px]">
+					<HomeInteractive />
+
+					{/* Upper left line */}
+					<svg className="not-lg:hidden full -left-290 absolute bottom-[70%] -z-10 max-h-[756px]" viewBox="0 0 1552 756" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M 1552 756 L 1284.28 210.94 C 1280.06 203.268 1272 198.5 1263.24 198.5 L 1088.88 198.5 C 1080.31 198.5 1072.39 193.926 1068.1 186.5 L 967.928 13 C 963.641 5.5744 955.718 1 947.144 1 H 0"
+							stroke="var(--color-soft)"
+						/>
+
+						<path
+							id="beamPath"
+							d="M 1552 756 L 1284.28 210.94 C 1280.06 203.268 1272 198.5 1263.24 198.5 L 1088.88 198.5 C 1080.31 198.5 1072.39 193.926 1068.1 186.5 L 967.928 13 C 963.641 5.5744 955.718 1 947.144 1 H 0"
+							fill="none"
+							stroke="var(--color-primary)"
+							strokeWidth="1"
+							strokeLinecap="round"
+							className="animate-[var(--animate-beam-flow)] [stroke-dasharray:50_1000] [stroke-dashoffset:0]"
+							pathLength="1000"
+						/>
+					</svg>
+
+					{/* Lower left line */}
+					<svg className="not-lg:hidden -left-290 absolute bottom-[75%] -z-10" width="1331" height="402" viewBox="0 0 1331 402" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M1329.73 401L1193.99 159.301C1190.79 153.591 1184.75 150.057 1178.2 150.057L1019.5 150.057C1013.03 150.057 1007.05 146.604 1003.81 141L928.21 10.0566C924.974 4.45235 918.995 1 912.523 1H-4"
+							stroke="var(--color-soft)"
+							strokeWidth="1"
+						/>
+						<path
+							id="beamPath"
+							d="M1329.73 401L1193.99 159.301C1190.79 153.591 1184.75 150.057 1178.2 150.057L1019.5 150.057C1013.03 150.057 1007.05 146.604 1003.81 141L928.21 10.0566C924.974 4.45235 918.995 1 912.523 1H-4"
+							fill="none"
+							stroke="var(--color-primary)"
+							strokeWidth="1"
+							strokeLinecap="round"
+							className="animate-[var(--animate-beam-flow2)] opacity-0 [stroke-dasharray:50_1000] [stroke-dashoffset:0]"
+							vectorEffect="non-scaling-stroke"
+							pathLength="1000"
+						/>
+					</svg>
+
+					{/* Upper right line */}
+					<svg
+						className="not-lg:hidden full -right-290 absolute bottom-[70%] -z-10 max-h-[756px] scale-x-[-1]"
+						viewBox="0 0 1552 756"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M 1552 756 L 1284.28 210.94 C 1280.06 203.268 1272 198.5 1263.24 198.5 L 1088.88 198.5 C 1080.31 198.5 1072.39 193.926 1068.1 186.5 L 967.928 13 C 963.641 5.5744 955.718 1 947.144 1 H 0"
+							stroke="var(--color-soft)"
+						/>
+
+						<path
+							id="beamPath"
+							d="M 1552 756 L 1284.28 210.94 C 1280.06 203.268 1272 198.5 1263.24 198.5 L 1088.88 198.5 C 1080.31 198.5 1072.39 193.926 1068.1 186.5 L 967.928 13 C 963.641 5.5744 955.718 1 947.144 1 H 0"
+							fill="none"
+							stroke="var(--color-primary)"
+							strokeWidth="1"
+							strokeLinecap="round"
+							className="animate-[var(--animate-beam-flow)] [stroke-dasharray:50_1000] [stroke-dashoffset:0]"
+							vectorEffect="non-scaling-stroke"
+							pathLength="1000"
+						/>
+					</svg>
+
+					{/* Lower right line */}
+					<svg
+						className="not-lg:hidden -right-290 absolute bottom-[75%] -z-10 scale-x-[-1]"
+						width="1331"
+						height="402"
+						viewBox="0 0 1331 402"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M1329.73 401L1193.99 159.301C1190.79 153.591 1184.75 150.057 1178.2 150.057L1019.5 150.057C1013.03 150.057 1007.05 146.604 1003.81 141L928.21 10.0566C924.974 4.45235 918.995 1 912.523 1H-4"
+							stroke="var(--color-soft)"
+							strokeWidth="1"
+						/>
+						<path
+							id="beamPath"
+							d="M1329.73 401L1193.99 159.301C1190.79 153.591 1184.75 150.057 1178.2 150.057L1019.5 150.057C1013.03 150.057 1007.05 146.604 1003.81 141L928.21 10.0566C924.974 4.45235 918.995 1 912.523 1H-4"
+							fill="none"
+							stroke="var(--color-primary)"
+							strokeWidth="1"
+							strokeLinecap="round"
+							className="animate-[var(--animate-beam-flow2)] opacity-0 [stroke-dasharray:50_1000] [stroke-dashoffset:0]"
+							vectorEffect="non-scaling-stroke"
+							pathLength="1000"
+						/>
+					</svg>
+				</div>
+			</Background>
+
+			{/* <FeaturesSection /> */}
+			<FeaturesSection />
+
+			<InvertedSection />
+
+			<ComponentsSection />
+
+			<PlaygroundSection />
+
+			<VideoSection />
+
+			<FAQSection />
+
+			<CTASection />
+
+			<FooterSection />
 		</div>
 	)
 }
