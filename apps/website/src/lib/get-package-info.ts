77f3a6e280bac40f9a4a28f3e3259d80a6b3d4eb
Ashmit72 <ashmitbastola@gmail.com>
2025-11-17 10:09:45 +0545

    chore: update version to match from npm package

--- apps/website/src/lib/get-package-info.ts
+++ apps/website/src/lib/get-package-info.ts
@@ -1,5 +1,6 @@
-import packageJson from "../../package.json"
-
-export const getPackageVersion = () => {
-	return packageJson.version
+export const getPackageVersion = async () => {
+	const version = await fetch(process.env.VERSION! || "https://registry.npmjs.org/radianui")
+		.then((res) => res.json())
+		.then((data) => data["dist-tags"].latest)
+	return version
 }
