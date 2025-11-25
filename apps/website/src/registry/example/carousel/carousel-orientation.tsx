c0311be69b4d2c0e220bf05de7acb59040cd4f26
yube01 <yubraja008@gmail.com>
2025-11-25 14:04:12 +0545

    chore: carousel example added

---
+++ apps/website/src/registry/example/carousel/carousel-orientation.tsx
@@ -0,0 +1,52 @@
+import * as React from "react"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/registry/ui/carousel"
+
+export default function CarouselOrientation() {
+	const images = [
+		{
+			src: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4",
+			alt: "Mountain landscape",
+		},
+		{
+			src: "https://images.unsplash.com/photo-1469474968028-56623f02e42e",
+			alt: "Nature scenery",
+		},
+		{
+			src: "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05",
+			alt: "Foggy forest",
+		},
+		{
+			src: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e",
+			alt: "Forest path",
+		},
+		{
+			src: "https://images.unsplash.com/photo-1426604966848-d7adac402bff",
+			alt: "Valley view",
+		},
+	]
+
+	return (
+		<div className="flex h-[500px] items-center justify-center">
+			<Carousel
+				opts={{
+					align: "start",
+				}}
+				orientation="vertical"
+				className="w-full max-w-xs">
+				<CarouselContent className="-mt-1 h-[360px]">
+					{images.map((image, index) => (
+						<CarouselItem key={index} className="basis-1/2 pb-1">
+							<div>
+								<div className="flex h-full items-center justify-center overflow-hidden rounded-lg">
+									<img src={image.src} alt={image.alt} className="h-[150px] w-full object-cover" />
+								</div>
+							</div>
+						</CarouselItem>
+					))}
+				</CarouselContent>
+				<CarouselPrevious />
+				<CarouselNext />
+			</Carousel>
+		</div>
+	)
+}
