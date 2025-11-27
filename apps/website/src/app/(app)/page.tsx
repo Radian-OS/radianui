f96d910216f39187146ca448a048e99e9fe5bfe2
Ashmit72 <ashmitbastola@gmail.com>
2025-11-27 12:44:19 +0545

    chore: fetaures section particles update

--- apps/website/src/app/(app)/page.tsx
+++ apps/website/src/app/(app)/page.tsx
@@ -143,17 +143,7 @@ export default function Page() {
 				</div>
 			</Background>
 
-			<FeaturesSection
-				textAutoHide={true}
-				enableStars={true}
-				enableSpotlight={true}
-				enableBorderGlow={true}
-				enableTilt={false}
-				enableMagnetism={false}
-				clickEffect={true}
-				spotlightRadius={573}
-				particleCount={12}
-			/>
+			<FeaturesSection textAutoHide={true} enableSpotlight={true} enableBorderGlow={true} enableTilt={false} enableMagnetism={false} clickEffect={true} spotlightRadius={573} />
 
 			<InvertedSection />
 
