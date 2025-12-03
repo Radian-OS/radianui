7ca40167d092fc07b22ccd36daf74cb37f8dabac
Ashmit72 <ashmitbastola@gmail.com>
2025-12-03 20:18:05 +0545

    chore: card aspect ratio example update

--- apps/website/src/registry/example/aspect-ratio/aspect-ratio-example1.tsx
+++ apps/website/src/registry/example/aspect-ratio/aspect-ratio-example1.tsx
@@ -5,8 +5,8 @@ import { AspectRatio } from "@/registry/ui/aspect-ratio"
 export default function AspectRatioPreview() {
 	return (
 		<div className="w-full max-w-[190px]">
-			<AspectRatio ratio={9 / 16} className="bg-fill1 overflow-hidden rounded-xl">
-				<Image src={"/media/background-2.png"} alt="Aspect Ratio Preview" fill className="object-cover" />
+			<AspectRatio ratio={9 / 16} className="bg-fill1 rounded-xl">
+				<Image src={"/media/background-2.png"} alt="Aspect Ratio Preview" fill className="rounded-xl object-cover" />
 			</AspectRatio>
 		</div>
 	)
--- apps/website/src/registry/example/card/card-info-example.tsx
+++ apps/website/src/registry/example/card/card-info-example.tsx
@@ -1,8 +1,7 @@
 import React from "react"
-import { FileStack, Link } from "lucide-react"
+import { FileStack, Link, ShieldAlert } from "lucide-react"
 import { Button } from "@/registry/ui/button"
-import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/registry/ui/card"
-import { Spinner } from "@/registry/ui/spinner"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/registry/ui/card"
 
 const CardInfoExample = () => {
 	return (
@@ -10,31 +9,35 @@ const CardInfoExample = () => {
 			<CardHeader>
 				<CardTitle>Sync In Progress</CardTitle>
 				<CardDescription>Started 3 mins ago</CardDescription>
-				<CardAction>
-					<Spinner variant="activity" className="text-info size-6" />
-				</CardAction>
 			</CardHeader>
-			<CardContent className="flex flex-col gap-2">
-				<div className="flex items-center justify-between">
+			<CardContent>
+				<div className="mb-2 flex items-center justify-between">
 					<div className="flex items-center gap-2">
 						<Link className="text-fg-secondary" size={20} />
 						<span className="text-fg-secondary text-sm">Events Found</span>
 					</div>
-					<span className="text-base font-medium">28</span>
+					<span className="text-sm font-medium">28</span>
+				</div>
+				<div className="mb-2 flex items-center justify-between">
+					<div className="flex items-center gap-2">
+						<ShieldAlert className="text-fg-secondary" size={20} />
+						<span className="text-fg-secondary text-sm">Events Expired</span>
+					</div>
+					<span className="text-sm font-medium">8</span>
 				</div>
 				<div className="flex items-center justify-between">
 					<div className="flex items-center gap-2">
 						<FileStack className="text-fg-secondary" size={20} />
 						<span className="text-fg-secondary text-sm">Sources Checked</span>
 					</div>
-					<span className="text-base font-medium">210 out of 340</span>
+					<span className="text-sm font-medium">210 out of 340</span>
 				</div>
 			</CardContent>
-			<CardFooter className="flex items-center gap-2">
-				<Button>Continue</Button>
+			<CardFooter className="flex items-center justify-end gap-2">
 				<Button variant="outline" color="neutral">
 					Cancel
 				</Button>
+				<Button>Continue</Button>
 			</CardFooter>
 		</Card>
 	)
