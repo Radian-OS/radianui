288de1c49186aa02abff21707516264d22423224
Be-En-AyA <bt.binaya@gmail.com>
2025-12-11 09:46:18 +0545

    chore: resend error resolving

--- apps/website/src/app/actions/subscribe/index.ts
+++ apps/website/src/app/actions/subscribe/index.ts
@@ -1,9 +1,7 @@
 "use server"
 
-// import { render } from "@react-email/components"
 import { Resend } from "resend"
-
-// import WelcomeEmailTemplate from "@/components/email/welcome-email-template"
+import WelcomeEmailTemplate from "@/components/email/welcome-email-template"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
@@ -24,18 +22,24 @@ export async function subscribe(email: string) {
 			return { message: error.message, status: 500 }
 		}
 
-		try {
-			await resend.emails.send({
-				from: process.env.RESEND_FROM_EMAIL!,
-				to: email,
-				subject: "Welcome to RadianOS",
-				// react: render(WelcomeEmailTemplate({ baseUrl: process.env.NEXT_PUBLIC_WEBSITE_URL! })),
-				text: "Hello, world!",
-			})
-		} catch (emailError) {
+		const { error: emailError } = await resend.emails.send({
+			from: process.env.RESEND_FROM_EMAIL!,
+			to: email,
+			subject: "Welcome to RadianOS",
+			react: WelcomeEmailTemplate({
+				baseUrl: process.env.NEXT_PUBLIC_WEBSITE_URL!,
+			}),
+		})
+
+		if (emailError) {
 			console.error("Failed to send welcome email:", emailError)
+			return {
+				message: "Contact added but failed to send welcome email",
+				status: 500,
+			}
 		}
 
+		console.log("email sent successfully")
 		return { message: "Email subscribed successfully", status: 201 }
 	} catch (error) {
 		console.error("Failed to subscribe email:", error)
