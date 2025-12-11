297a20ad102cfd796317ba628d0e6cc80d93be8f
Be-En-AyA <bt.binaya@gmail.com>
2025-12-11 09:59:35 +0545

    chore: resend too many request problem resolved with delay

--- apps/website/src/app/actions/subscribe/index.ts
+++ apps/website/src/app/actions/subscribe/index.ts
@@ -5,6 +5,8 @@ import WelcomeEmailTemplate from "@/components/email/welcome-email-template"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
 export async function subscribe(email: string) {
 	try {
 		const { data } = await resend.contacts.get({ email: email })
@@ -13,6 +15,7 @@ export async function subscribe(email: string) {
 			return { message: "Your email is already subscribed", status: 409 }
 		}
 
+		await delay(1000)
 		const { error } = await resend.contacts.create({
 			email: email,
 			unsubscribed: false,
@@ -22,6 +25,7 @@ export async function subscribe(email: string) {
 			return { message: error.message, status: 500 }
 		}
 
+		await delay(1000)
 		const { error: emailError } = await resend.emails.send({
 			from: process.env.RESEND_FROM_EMAIL!,
 			to: email,
