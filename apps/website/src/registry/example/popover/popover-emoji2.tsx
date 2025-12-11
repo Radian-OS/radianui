458833471b194c33acc62c216d8b70e497d5835d
Ashmit72 <ashmitbastola@gmail.com>
2025-12-11 12:21:29 +0545

    fix: remove shadcn colors and implement radian colors

--- apps/website/src/registry/example/popover/popover-emoji2.tsx
+++ apps/website/src/registry/example/popover/popover-emoji2.tsx
@@ -28,7 +28,7 @@ export default function PopoverEmoji2() {
 					</div>
 					<p className="text-fg-tertiary text-xs font-normal">radian_profile_1146.jpg</p>
 				</div>
-				<img height={170} width={300} src="/media/background-2.png" className="rounded-md" />
+				<img height={170} width={300} src="/media/background-2.png" alt="Background2" className="rounded-md" />
 				<div className="max-w-75 flex flex-wrap gap-1.5">
 					{selectedEmoji.map((emoji) => (
 						<Button variant="outline" color="neutral" size="28" key={emoji} onClick={() => setSelectedEmoji((prev) => prev.filter((e) => e !== emoji))}>
