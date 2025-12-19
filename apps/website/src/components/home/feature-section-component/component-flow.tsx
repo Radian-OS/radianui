4475495600e930f57dd5ddb45c5f94f12dc37ba7
yube01 <yubraja008@gmail.com>
2025-12-19 15:17:37 +0545

    fix: responsive fix in light beam

--- apps/website/src/components/home/feature-section-component/component-flow.tsx
+++ apps/website/src/components/home/feature-section-component/component-flow.tsx
@@ -10,9 +10,9 @@ export function ComponentFlowCard({ gridRef }: ComponentFlowCardProps) {
 	return (
 		<div className="h-full w-full overflow-hidden pt-2 sm:pt-3 md:pt-5">
 			<BentoCardGrid gridRef={gridRef} className="flex flex-col items-center">
-				<div className="relative flex w-full items-center justify-center">
+				<div className="relative flex w-[930px] items-center justify-center lg:w-full">
 					{/* Left side SVGs */}
-					<div className="absolute -top-1 left-0 z-0 flex origin-left flex-col gap-[38px] min-[40px]:scale-x-[20%] min-[40px]:scale-y-[60%] min-[500px]:scale-x-[70%] min-[500px]:scale-y-[80%] sm:scale-x-[75%] sm:scale-y-[85%] md:scale-x-[110%] md:scale-y-[95%] lg:scale-x-100 lg:scale-y-100">
+					<div className="absolute -top-1 left-0 z-0 flex origin-left scale-x-[110%] scale-y-[95%] flex-col gap-[38px] lg:scale-100">
 						<svg xmlns="http://www.w3.org/2000/svg" width="212" height="103" viewBox="0 0 212 103" fill="none">
 							<path
 								d="M-8.91712e-06 0.5L68.4891 0.499994C72.7181 0.499994 76.6348 2.726 78.7989 6.35934L132.572 96.6406C134.736 100.274 138.652 102.5 142.881 102.5L212 102.5"
@@ -100,12 +100,12 @@ export function ComponentFlowCard({ gridRef }: ComponentFlowCardProps) {
 					</div>
 
 					{/* Center ComponentUI */}
-					<div className="z-1 relative scale-[65%] sm:scale-[80%] md:scale-[110%] lg:scale-100">
+					<div className="z-1 relative scale-[110%] lg:scale-100">
 						<ComponentsUI />
 					</div>
 
 					{/* Right side SVGs */}
-					<div className="absolute -top-1 right-0 z-0 flex origin-right flex-col gap-[38px] min-[40px]:scale-x-[20%] min-[40px]:scale-y-[60%] min-[500px]:scale-x-[70%] min-[500px]:scale-y-[80%] sm:scale-x-[75%] sm:scale-y-[85%] md:scale-x-[110%] md:scale-y-[95%] lg:scale-x-100 lg:scale-y-100">
+					<div className="absolute -top-1 right-0 z-0 flex origin-right scale-x-[110%] scale-y-[95%] flex-col gap-[38px] lg:scale-100">
 						<svg width="212" height="103" viewBox="0 0 212 103" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M0 102.5H68.4891C72.7181 102.5 76.6348 100.274 78.7989 96.6407L132.572 6.35935C134.736 2.72601 138.652 0.5 142.881 0.5H212" stroke="var(--color-soft)" />
 							<path
