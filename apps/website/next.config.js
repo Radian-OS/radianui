ec345ce119c6f0fecf7f6651d2cd0d98723ec274
Bijay Khapung <khapungbj84@gmail.com>
2025-12-23 12:27:54 +0545

    perf: optimize avatar images and dynamically load features section

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -59,28 +59,6 @@ const nextConfig = {
 	compiler: {
 		removeConsole: false,
 	},
-	webpack(config) {
-		// Grab the existing rule that handles SVG imports
-		const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"))
-		config.module.rules.push(
-			// Reapply the existing rule, but only for svg imports ending in ?url
-			{
-				...fileLoaderRule,
-				test: /\.svg$/i,
-				resourceQuery: /url/, // *.svg?url
-			},
-			// Convert all other *.svg imports to React components
-			{
-				test: /\.svg$/i,
-				issuer: fileLoaderRule.issuer,
-				resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-				use: ["@svgr/webpack"],
-			}
-		)
-		// Modify the file loader rule to ignore *.svg, since we have it handled now.
-		fileLoaderRule.exclude = /\.svg$/i
-		return config
-	},
 	env: {
 		RESEND_API_KEY: process.env.RESEND_API_KEY,
 		RESEND_FROM_EMAIL: process.env.RESEND_FROM_EMAIL,
