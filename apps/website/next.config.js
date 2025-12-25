69d51ce58731739941363a4f4c07945de1d1c217
Ashmit72 <ashmitbastola@gmail.com>
2025-12-25 12:33:33 +0545

    fix: seo warnings

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -7,6 +7,28 @@ const withBundleAnalyzer = bundleAnalyzer({
 })
 
 const nextConfig = {
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: [
+					{
+						key: "Referrer-Policy",
+						value: "strict-origin-when-cross-origin",
+					},
+					{
+						key: "Strict-Transport-Security",
+						value: "max-age=31536000; includeSubDomains",
+					},
+					{
+						key: "X-Content-Type-Options",
+						value: "nosniff",
+					},
+				],
+			},
+		]
+	},
+
 	async rewrites() {
 		return [
 			{
