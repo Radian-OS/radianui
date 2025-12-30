c113a60a8daa7b5a0e6d06f4d9f9a8791c650a26
Bijay Khapung <khapungbj84@gmail.com>
2025-12-30 11:59:49 +0545

    fix: minor accessibility issue

--- apps/website/src/components/home/playground-section.tsx
+++ apps/website/src/components/home/playground-section.tsx
@@ -190,7 +190,7 @@ export default function PlaygroundSection() {
 									</div>
 									<div className="border-border flex h-8 items-center border-l pl-2">
 										<div className="text-fg-secondary flex">
-											<button aria-label="Toggle Theme" onClick={toggleTheme} className="hover:bg-fill2 a flex size-8 cursor-pointer items-center justify-center rounded-md">
+											<button aria-label="Toggle Theme" onClick={toggleTheme} className="hover:bg-fill2 flex size-8 cursor-pointer items-center justify-center rounded-md">
 												{theme === "light" ? <MoonIcon size={18} /> : <SunIcon size={18} />}
 											</button>
 											<Colors />
