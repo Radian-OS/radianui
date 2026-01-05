7f3c38da079f4cbfd1929428a41b815ce7e15dfa
Be-En-AyA <bt.binaya@gmail.com>
2026-01-05 10:13:07 +0545

    chore(website): excluded the blocks/* urls from sitemap

--- apps/website/next-sitemap.config.cjs
+++ apps/website/next-sitemap.config.cjs
@@ -3,6 +3,7 @@ const isPublic = process.env.NEXT_PUBLIC_DEPLOYMENT_ENV === "PROD"
 module.exports = {
 	siteUrl: process.env.NEXT_PUBLIC_WEBSITE_URL,
 	generateRobotsTxt: true,
+	exclude: ["/blocks/*"],
 	robotsTxtOptions: {
 		policies: [
 			{
