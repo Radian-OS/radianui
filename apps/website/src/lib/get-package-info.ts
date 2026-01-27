e94ea8912ba051d9b8444c1c82981c2822aa78ec
Be-En-AyA <bt.binaya@gmail.com>
2026-01-27 11:22:22 +0545

    perf(website): get-package-info cache set to no-store and removed revalidate

--- apps/website/src/lib/get-package-info.ts
+++ apps/website/src/lib/get-package-info.ts
@@ -1,6 +1,6 @@
 export const getPackageVersion = async () => {
 	const version = await fetch("https://registry.npmjs.org/radianui", {
-		next: { revalidate: false },
+		cache: "no-store",
 	})
 		.then((res) => res.json())
 		.then((data) => data["dist-tags"].latest)
