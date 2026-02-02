fd84ebda1ac1ba30b96cf055369021b86eb65cde
Be-En-AyA <bt.binaya@gmail.com>
2026-02-02 12:02:47 +0545

    style: copy terminal command  toast styles updated

--- apps/website/src/components/home/hero-action-buttons.tsx
+++ apps/website/src/components/home/hero-action-buttons.tsx
@@ -21,11 +21,11 @@ export default function HeroActionButtons() {
 					copy(e)
 
 					toast.custom(() => (
-						<div className="bg-black-inverse text-fg-inverse flex w-[416px] items-center gap-2 rounded-lg px-3 py-2.5">
+						<div className="bg-black-inverse text-fg-inverse sm:w-75 flex w-full items-center gap-2 rounded-lg px-3 py-2.5 shadow-[0_16px_24px_-4px_rgba(25,24,27,0.12)]">
 							<Check size={20} className="text-success" />
-							<div className="text-fg-inverse">
-								<p className="text-sm font-medium">Copied Command:</p>
-								<p className="text-sm font-normal">{TERMINAL_COMMAND}</p>
+							<div className="text-fg-inverse space-y-0.5 text-sm">
+								<p className="font-semibold">Command copied to clipboard</p>
+								<p>{TERMINAL_COMMAND}</p>
 							</div>
 						</div>
 					))
