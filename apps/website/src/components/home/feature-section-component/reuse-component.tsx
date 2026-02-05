5a062f2634b151a39b65bd3815a6dcef6c1a5fc6
yube01 <yubraja008@gmail.com>
2026-02-05 09:27:41 +0545

    fix: reuseable float animation removed and image update

--- apps/website/src/components/home/feature-section-component/reuse-component.tsx
+++ apps/website/src/components/home/feature-section-component/reuse-component.tsx
@@ -30,25 +30,6 @@ export function ReuseComponent() {
 		setCenterCardIndex(cardIndex)
 	}
 
-	// Start continuous up-down animation
-	const startFloatingAnimation = async () => {
-		// First, smoothly move to starting position if not already there
-		await controls.start({
-			y: -50,
-			transition: { duration: 1, ease: "easeOut" },
-		})
-
-		// Then start the infinite loop
-		controls.start({
-			y: [-50, -150, -50],
-			transition: {
-				duration: 4,
-				repeat: Infinity,
-				ease: "easeInOut",
-			},
-		})
-	}
-
 	useEffect(() => {
 		const container = containerRef.current
 		if (container) {
@@ -58,7 +39,6 @@ export function ReuseComponent() {
 			scrollX.set(initialScroll)
 		}
 
-		startFloatingAnimation()
 		calculateCenterCard()
 	}, [])
 
@@ -70,7 +50,6 @@ export function ReuseComponent() {
 			// When velocity is near zero, animation is complete
 			if (Math.abs(velocity) < 1 && isAnimating) {
 				setIsAnimating(false)
-				startFloatingAnimation()
 			}
 		})
 
@@ -155,50 +134,50 @@ export function ReuseComponent() {
 		{
 			id: 0,
 			type: {
-				darkmode: "/reuseable-ui/dark-mode3.png",
-				lightmode: "/reuseable-ui/light-mode3.png",
+				darkmode: "",
+				lightmode: "",
 			},
 		},
 		{
 			id: 1,
 			type: {
-				darkmode: "/reuseable-ui/dark-mode2.png",
-				lightmode: "/reuseable-ui/light-mode2.png",
+				darkmode: "",
+				lightmode: "",
 			},
 		},
 		{
 			id: 2,
 			type: {
+				darkmode: "",
+				lightmode: "",
+			},
+		},
+		{
+			id: 3,
+			type: {
 				darkmode: "/reuseable-ui/dark-mode3.png",
 				lightmode: "/reuseable-ui/light-mode3.png",
 			},
 		},
 		{
-			id: 3,
+			id: 4,
 			type: {
 				darkmode: "/reuseable-ui/dark-mode2.png",
 				lightmode: "/reuseable-ui/light-mode2.png",
 			},
 		},
 		{
-			id: 4,
+			id: 5,
 			type: {
 				darkmode: "/reuseable-ui/dark-mode1.png",
 				lightmode: "/reuseable-ui/light-mode1.png",
-			},
-		},
-		{
-			id: 5,
-			type: {
-				darkmode: "/reuseable-ui/dark-mode3.png",
-				lightmode: "/reuseable-ui/light-mode3.png",
 			},
 		},
 		{
 			id: 6,
 			type: {
-				darkmode: "/reuseable-ui/dark-mode1.png",
-				lightmode: "/reuseable-ui/light-mode1.png",
+				darkmode: "",
+				lightmode: "",
 			},
 		},
 	]
@@ -219,7 +198,7 @@ export function ReuseComponent() {
 					<div
 						onMouseMove={(e) => handleMouseMove(e, card.id)}
 						key={card.id}
-						className={`w-90 relative flex h-[520px] flex-shrink-0 flex-col items-center justify-center transition-all duration-300`}>
+						className={`w-90 relative flex h-[580px] flex-shrink-0 flex-col items-center justify-center transition-all duration-300`}>
 						<AnimatePresence mode="sync">
 							{centerCardIndex === index - 2 ? (
 								<motion.div
@@ -231,22 +210,40 @@ export function ReuseComponent() {
 										duration: 0.5,
 										ease: "linear",
 									}}
-									className="absolute inset-0 flex items-center justify-center">
-									<Image src={card.type.darkmode} className="not-dark:hidden" alt="Dark Mode 1" width={360} height={594} />
-									<Image src={card.type.lightmode} className="dark:hidden" alt="Dark Mode 1" width={360} height={594} />
+									className="absolute inset-0 flex items-start justify-center">
+									{card.type.darkmode && <Image unoptimized src={card.type.darkmode} className="not-dark:hidden" alt="Dark Mode" width={360} height={594} />}
+									{card.type.lightmode && <Image unoptimized src={card.type.lightmode} className="dark:hidden" alt="Light Mode" width={360} height={594} />}
 								</motion.div>
 							) : (
-								<motion.div
-									key={`empty-${index}`}
-									initial={{ opacity: 0 }}
-									animate={{ opacity: 1 }}
-									exit={{ opacity: 0 }}
-									transition={{
-										duration: 0.5,
-										ease: "linear",
-									}}
-									className="w-90 bg-fill1 absolute inset-0 flex flex-col items-center justify-center rounded-2xl border-2 border-dotted px-6 py-8"
-								/>
+								<>
+									{!card.type.darkmode || !card.type.lightmode ? (
+										<motion.div
+											key={`empty1-${index}`}
+											initial={{ opacity: 0 }}
+											animate={{ opacity: 1 }}
+											exit={{ opacity: 0 }}
+											transition={{
+												duration: 0.5,
+												ease: "linear",
+											}}
+											className="w-90 bg-fill1 absolute inset-0 flex flex-col items-center justify-center rounded-2xl border-2 border-dotted px-6 py-8 opacity-10"
+										/>
+									) : (
+										<motion.div
+											key={`empty-${index}`}
+											initial={{ opacity: 0 }}
+											animate={{ opacity: 1 }}
+											exit={{ opacity: 0 }}
+											transition={{
+												duration: 0.5,
+												ease: "linear",
+											}}
+											className="absolute inset-0 flex items-start justify-center">
+											{card.type.darkmode && <Image unoptimized src={card.type.darkmode} className="not-dark:hidden opacity-20" alt="Dark Mode" width={360} height={594} />}
+											{card.type.lightmode && <Image unoptimized src={card.type.lightmode} className="opacity-20 dark:hidden" alt="Light Mode" width={360} height={594} />}
+										</motion.div>
+									)}
+								</>
 							)}
 						</AnimatePresence>
 					</div>
