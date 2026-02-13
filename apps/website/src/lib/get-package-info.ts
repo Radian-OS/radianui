e36dcd8536e662ff338479f09ca9d63120b6a321
yube01 <yubraja008@gmail.com>
2026-02-13 13:27:10 +0545

    fix: build error

--- apps/website/src/lib/get-package-info.ts
+++ apps/website/src/lib/get-package-info.ts
@@ -1,6 +1,6 @@
 export const getPackageVersion = async () => {
 	const version = await fetch("https://registry.npmjs.org/radianui", {
-		next: { revalidate: 3600 }, // cache for 1 hour
+		cache: "no-store",
 	})
 		.then((res) => res.json())
 		.then((data) => data["dist-tags"].latest)
