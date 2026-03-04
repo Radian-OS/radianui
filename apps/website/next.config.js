9d5179b5dbcc8acd1d018f65e6af34768c0c0658
Bijay Khapung <khapungbj84@gmail.com>
2026-03-04 11:29:10 +0545

    chore: change cache header

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -41,6 +41,19 @@ const nextConfig = {
 						key: "Permissions-Policy",
 						value: "camera=(), microphone=(), geolocation=()",
 					},
+					{
+						key: "Cache-Control",
+						value: "public, max-age=120, stale-while-revalidate=30",
+					},
+				],
+			},
+			{
+				source: "/docs/:path*",
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "public, max-age=31536000, immutable",
+					},
 				],
 			},
 		]
