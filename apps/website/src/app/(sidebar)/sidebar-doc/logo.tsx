f71b7d61e4a72cb4846232e2ffd80212f8561fe7
Bijay Khapung <khapungbj84@gmail.com>
2026-03-04 15:37:16 +0545

    chore: merge main

---
+++ apps/website/src/app/(sidebar)/sidebar-doc/logo.tsx
@@ -0,0 +1,179 @@
+export default function Logo() {
+	return (
+		<svg
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg">
+			<g filter="url(#filter0_iii_13964_1836)">
+				<path
+					d="M0 6C0 2.68629 2.68629 0 6 0H18C21.3137 0 24 2.68629 24 6V18C24 21.3137 21.3137 24 18 24H6C2.68629 24 0 21.3137 0 18V6Z"
+					fill="#3D99F5"
+				/>
+				<path
+					d="M0 6C0 2.68629 2.68629 0 6 0H18C21.3137 0 24 2.68629 24 6V18C24 21.3137 21.3137 24 18 24H6C2.68629 24 0 21.3137 0 18V6Z"
+					fill="url(#paint0_linear_13964_1836)"
+				/>
+				<path
+					d="M6 0.5H18C21.0376 0.5 23.5 2.96243 23.5 6V18C23.5 21.0376 21.0376 23.5 18 23.5H6C2.96243 23.5 0.5 21.0376 0.5 18V6C0.5 2.96243 2.96243 0.5 6 0.5Z"
+					stroke="url(#paint1_linear_13964_1836)"
+				/>
+				<g filter="url(#filter1_d_13964_1836)">
+					<path
+						d="M8.75 12H8.77539L7.125 10.3242L5.5 8.75V5.5H8.75L12 8.75V15.25L10.375 16.8242L8.72461 18.5H5.5V15.25L8.75 12ZM18.5 8.75V15.25L16.875 16.8242L15.2246 18.5H12V15.25L15.25 12H15.2754L13.625 10.3242L12 8.75V5.5H15.25L18.5 8.75Z"
+						fill="url(#paint2_linear_13964_1836)"
+						shapeRendering="crispEdges"
+					/>
+				</g>
+			</g>
+			<defs>
+				<filter
+					id="filter0_iii_13964_1836"
+					x="0"
+					y="-1"
+					width="24"
+					height="26"
+					filterUnits="userSpaceOnUse"
+					colorInterpolationFilters="sRGB">
+					<feFlood floodOpacity="0" result="BackgroundImageFix" />
+					<feBlend
+						mode="normal"
+						in="SourceGraphic"
+						in2="BackgroundImageFix"
+						result="shape"
+					/>
+					<feColorMatrix
+						in="SourceAlpha"
+						type="matrix"
+						values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+						result="hardAlpha"
+					/>
+					<feMorphology
+						radius="0.5"
+						operator="erode"
+						in="SourceAlpha"
+						result="effect1_innerShadow_13964_1836"
+					/>
+					<feOffset />
+					<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+					<feColorMatrix
+						type="matrix"
+						values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0"
+					/>
+					<feBlend
+						mode="normal"
+						in2="shape"
+						result="effect1_innerShadow_13964_1836"
+					/>
+					<feColorMatrix
+						in="SourceAlpha"
+						type="matrix"
+						values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+						result="hardAlpha"
+					/>
+					<feOffset dy="1" />
+					<feGaussianBlur stdDeviation="0.5" />
+					<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+					<feColorMatrix
+						type="matrix"
+						values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.1 0"
+					/>
+					<feBlend
+						mode="normal"
+						in2="effect1_innerShadow_13964_1836"
+						result="effect2_innerShadow_13964_1836"
+					/>
+					<feColorMatrix
+						in="SourceAlpha"
+						type="matrix"
+						values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+						result="hardAlpha"
+					/>
+					<feOffset dy="-1" />
+					<feGaussianBlur stdDeviation="0.5" />
+					<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+					<feColorMatrix
+						type="matrix"
+						values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0"
+					/>
+					<feBlend
+						mode="normal"
+						in2="effect2_innerShadow_13964_1836"
+						result="effect3_innerShadow_13964_1836"
+					/>
+				</filter>
+				<filter
+					id="filter1_d_13964_1836"
+					x="4.54031"
+					y="5.26008"
+					width="14.9194"
+					height="14.9194"
+					filterUnits="userSpaceOnUse"
+					colorInterpolationFilters="sRGB">
+					<feFlood floodOpacity="0" result="BackgroundImageFix" />
+					<feColorMatrix
+						in="SourceAlpha"
+						type="matrix"
+						values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+						result="hardAlpha"
+					/>
+					<feMorphology
+						radius="0.479847"
+						operator="erode"
+						in="SourceAlpha"
+						result="effect1_dropShadow_13964_1836"
+					/>
+					<feOffset dy="0.719771" />
+					<feGaussianBlur stdDeviation="0.719771" />
+					<feComposite in2="hardAlpha" operator="out" />
+					<feColorMatrix
+						type="matrix"
+						values="0 0 0 0 0.141176 0 0 0 0 0.141176 0 0 0 0 0.141176 0 0 0 0.1 0"
+					/>
+					<feBlend
+						mode="normal"
+						in2="BackgroundImageFix"
+						result="effect1_dropShadow_13964_1836"
+					/>
+					<feBlend
+						mode="normal"
+						in="SourceGraphic"
+						in2="effect1_dropShadow_13964_1836"
+						result="shape"
+					/>
+				</filter>
+				<linearGradient
+					id="paint0_linear_13964_1836"
+					x1="12"
+					y1="2.98023e-07"
+					x2="13"
+					y2="24"
+					gradientUnits="userSpaceOnUse">
+					<stop stopColor="white" stopOpacity="0" />
+					<stop offset="1" stopColor="white" stopOpacity="0.15" />
+				</linearGradient>
+				<linearGradient
+					id="paint1_linear_13964_1836"
+					x1="12"
+					y1="0"
+					x2="12"
+					y2="24"
+					gradientUnits="userSpaceOnUse">
+					<stop stopColor="white" stopOpacity="0.2" />
+					<stop offset="1" stopColor="white" stopOpacity="0" />
+				</linearGradient>
+				<linearGradient
+					id="paint2_linear_13964_1836"
+					x1="12"
+					y1="5.5"
+					x2="12"
+					y2="18.5"
+					gradientUnits="userSpaceOnUse">
+					<stop stopColor="white" />
+					<stop offset="1" stopColor="white" stopOpacity="0.5" />
+				</linearGradient>
+			</defs>
+		</svg>
+	)
+}
