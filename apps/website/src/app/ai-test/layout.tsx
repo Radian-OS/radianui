70d3725f89a61511d4305b00afc0ee50c990456f
yube01 <yubraja008@gmail.com>
2026-03-19 11:38:19 +0545

    fix: theme toggle position on center

--- apps/website/src/app/ai-test/layout.tsx
+++ apps/website/src/app/ai-test/layout.tsx
@@ -11,9 +11,11 @@ export default function BlocksLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<>
-			<DesktopThemeToggler />
+		<main className="relative w-full">
+			<div className="z-99 absolute left-1/2 top-0 -translate-x-1/2 transform">
+				<DesktopThemeToggler />
+			</div>
 			{children}
-		</>
+		</main>
 	)
 }
