2593ebc2723114d3118bbec49184b01a8af6e521
yube01 <yubraja008@gmail.com>
2026-04-01 12:29:24 +0545

    test: svg import for blocks

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -19,6 +19,10 @@ const nextConfig = {
 				],
 			},
 			{
+				source: "/icons/:path*",
+				headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+			},
+			{
 				source: "/docs/:path*",
 				headers: [
 					{
