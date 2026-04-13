825bba2d35d3491ad86293483a5e89204db29be9
yube01 <yubraja008@gmail.com>
2026-04-13 08:33:38 +0545

    fix: cli font issue

--- templates/next-app/app/page.tsx
+++ templates/next-app/app/page.tsx
@@ -3,7 +3,7 @@ export default function Page() {
 	return (
 		<div className="flex min-h-svh p-6">
 			<div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-				<div className="font-mono text-xs text-muted-foreground">
+				<div className="text-xs text-muted-foreground">
 					(Press <kbd>d</kbd> to toggle dark mode)
 				</div>
 			</div>
