d337180ef7a82ae731bed437b7a16e5b8fd19492
yube01 <yubraja008@gmail.com>
2026-05-15 01:27:33 +0000

    fix: signup, new password and sidebar updated

--- apps/website/src/registry/blocks-example/new-password/components/new-password-form.tsx
+++ apps/website/src/registry/blocks-example/new-password/components/new-password-form.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import React, { useMemo, useRef, useState } from "react"
+import React, { useRef, useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import z from "zod"
+import { z } from "zod"
 import { IconSlot } from "@/registry/icon-library"
 import { Button } from "@/registry/ui/button"
 import {
@@ -15,7 +15,7 @@ import {
 	FormMessage,
 } from "@/registry/ui/form"
 import { Input, InputWrapper } from "@/registry/ui/input"
-import { Progress } from "@/registry/ui/progress"
+import { Spinner } from "@/registry/ui/spinner"
 import Logo from "./brand-logo"
 
 const FormSchema = z
@@ -23,22 +23,16 @@ const FormSchema = z
 		password: z
 			.string()
 			.min(8, "Password must be at least 8 characters long")
-			.regex(/[a-z]/, "Password must contain at least one lowercase letter")
-			.regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-			.regex(/\d/, "Password must contain at least one number"),
+			.regex(
+				/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
+				"Password must contain at least one uppercase letter, one lowercase letter, and one number"
+			),
 		confirmPassword: z.string(),
 	})
 	.refine((data) => data.password === data.confirmPassword, {
 		message: "Passwords don't match",
 		path: ["confirmPassword"],
 	})
-
-const passwordSchema = z
-	.string()
-	.min(8, { message: "At least 8 characters" })
-	.regex(/\d/, { message: "At least one number" })
-	.regex(/[a-z]/, { message: "At least one lowercase letter" })
-	.regex(/[A-Z]/, { message: "At least one uppercase letter" })
 
 export default function NewPasswordForm() {
 	const [isLoading, setIsLoading] = useState(false)
@@ -52,7 +46,11 @@ export default function NewPasswordForm() {
 		setShowPassword(!showPassword)
 	}
 
-	const IconComponent = showPassword ? "eyeoff" : "eye"
+	const IconComponent = showPassword ? (
+		<IconSlot slot="eye" />
+	) : (
+		<IconSlot slot="eyeoff" />
+	)
 
 	const form = useForm<z.infer<typeof FormSchema>>({
 		resolver: zodResolver(FormSchema),
@@ -61,26 +59,6 @@ export default function NewPasswordForm() {
 			confirmPassword: "",
 		},
 	})
-
-	const password = form.watch("password")
-
-	const validation = useMemo(
-		() => passwordSchema.safeParse(password),
-		[password]
-	)
-
-	const errors = useMemo(() => {
-		if (validation.success) return []
-		return validation.error.issues.map((e) => e.message)
-	}, [validation])
-
-	const progress = useMemo(() => {
-		const totalChecks = 4
-		const passedChecks = totalChecks - errors.length
-		return (passedChecks / totalChecks) * 100
-	}, [errors])
-
-	const isValid = (message: string) => !errors.includes(message)
 
 	const onSubmit = (data: z.infer<typeof FormSchema>) => {
 		console.log(data)
@@ -93,113 +71,84 @@ export default function NewPasswordForm() {
 	}
 
 	return (
-		<div className="max-w-90 flex w-full flex-col">
-			<div className="pb-6">
-				<Logo />
-			</div>
-			<div className="flex flex-col gap-2 pb-8">
-				<span className="heading-5 font-semibold">Create a new password</span>
-				<span className="text-fg-secondary text-sm">
-					Enter a new password to change your password.
-				</span>
-			</div>
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)}>
-					<div className="flex flex-col gap-4">
-						<FormField
-							control={form.control}
-							name="password"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel htmlFor="password" className="font-medium">
-										Password
-									</FormLabel>
-									<FormControl>
-										<InputWrapper>
-											<Input
-												{...field}
-												id="password"
-												ref={inputRef}
-												className="peer"
-												type={showPassword ? "text" : "password"}
-											/>
-											<IconSlot
-												slot={IconComponent}
-												className="hover:text-fg peer-disabled:text-fg-disabled cursor-pointer peer-disabled:pointer-events-none"
-												onMouseDown={togglePasswordVisibility}
-											/>
-										</InputWrapper>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name="confirmPassword"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel
-										htmlFor="re-enter-password"
-										className="font-medium">
-										Re-enter new password
-									</FormLabel>
-									<div className="flex flex-col gap-4">
-										<FormControl>
-											<InputWrapper>
-												<Input
-													{...field}
-													id="re-enter-password"
-													ref={inputRef}
-													className="peer"
-													type={showPassword ? "text" : "password"}
-												/>
-												<IconSlot
-													slot={IconComponent}
-													className="hover:text-fg peer-disabled:text-fg-disabled cursor-pointer peer-disabled:pointer-events-none"
-													onMouseDown={togglePasswordVisibility}
-												/>
-											</InputWrapper>
-										</FormControl>
-										<div className="body-13 flex w-full flex-col gap-2">
-											<Progress value={progress} />
-											<p className="text-sm font-semibold">
-												Your Password must contain
-											</p>
-											{[
-												"At least 8 characters",
-												"At least one number",
-												"At least one lowercase letter",
-												"At least one uppercase letter",
-											].map((label) => (
-												<p
-													key={label}
-													className="text-fg-tertiary flex items-center gap-2">
-													<IconSlot
-														slot="check"
-														className={`size-4 ${isValid(label) ? "text-success-text" : ""}`}
+		<div className="w-100 bg-bg border-border flex rounded-2xl border px-6 py-8">
+			<div className="flex flex-1 flex-col gap-8">
+				<div className="flex flex-1 flex-col gap-6">
+					<div>
+						<Logo />
+					</div>
+					<div className="flex flex-col gap-2">
+						<h1 className="heading-5">Change Your Password</h1>
+						<p className="text-fg-secondary text-sm">
+							Enter a new password below to change your password.
+						</p>
+					</div>
+				</div>
+
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)}>
+						<div className="flex flex-col gap-5">
+							<div className="flex flex-col gap-4">
+								<FormField
+									control={form.control}
+									name="password"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>New password</FormLabel>
+											<FormControl>
+												<InputWrapper>
+													<Input
+														{...field}
+														id="toggle-visible-password"
+														ref={inputRef}
+														className="peer"
+														type={showPassword ? "text" : "password"}
 													/>
-													{label}
-												</p>
-											))}
-										</div>
-									</div>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<div className="py-1">
-							<Button
-								className="w-full"
-								type="submit"
-								loading={isLoading}
-								disabled={isLoading}>
-								{isLoading ? "Changing Password" : "Change password"}
+													{React.cloneElement(IconComponent, {
+														className:
+															"hover:text-fg peer-disabled:text-fg-disabled cursor-pointer peer-disabled:pointer-events-none",
+														onMouseDown: togglePasswordVisibility,
+													})}
+												</InputWrapper>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name="confirmPassword"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Re-enter new password</FormLabel>
+											<FormControl>
+												<InputWrapper>
+													<Input
+														{...field}
+														id="toggle-visible-password"
+														ref={inputRef}
+														className="peer"
+														type={showPassword ? "text" : "password"}
+													/>
+													{React.cloneElement(IconComponent, {
+														className:
+															"hover:text-fg peer-disabled:text-fg-disabled cursor-pointer peer-disabled:pointer-events-none",
+														onMouseDown: togglePasswordVisibility,
+													})}
+												</InputWrapper>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+							<Button className="w-full" type="submit" disabled={isLoading}>
+								{isLoading ? <Spinner variant="default" /> : "Reset password"}
 							</Button>
 						</div>
-					</div>
-				</form>
-			</Form>
+					</form>
+				</Form>
+			</div>
 		</div>
 	)
 }
--- apps/website/src/registry/blocks-example/new-password/page.tsx
+++ apps/website/src/registry/blocks-example/new-password/page.tsx
@@ -1,24 +1,9 @@
-import Image from "next/image"
 import NewPasswordForm from "./components/new-password-form"
 
 export default function Page() {
 	return (
-		<div className="bg-bg flex h-screen w-screen">
-			<div className="flex w-full">
-				<div className="hidden flex-1 lg:block">
-					<Image
-						className="h-full w-full object-cover"
-						src="/blocks/bg-4.png"
-						alt="Background Image"
-						width={960}
-						height={1024}
-						priority
-					/>
-				</div>
-				<div className="bg-bg flex h-full w-full flex-1 items-center justify-center p-5">
-					<NewPasswordForm />
-				</div>
-			</div>
+		<div className="bg-elevation-negative flex h-screen w-screen items-center justify-center px-5">
+			<NewPasswordForm />
 		</div>
 	)
 }
