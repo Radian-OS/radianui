42fdc3aab027f5eb87b1cc6e034d065bef5de7f2
Ashmit72 <ashmitbastola@gmail.com>
2026-05-20 14:08:23 +0545

    feat: add group/toggle className inside the cva

--- apps/website/src/registry/example/toggle/toggle-colors.tsx
+++ apps/website/src/registry/example/toggle/toggle-colors.tsx
@@ -6,110 +6,66 @@ const ToggleVariantsExample = () => {
 	return (
 		<div className="flex flex-col items-center justify-center gap-5">
 			<div className="flex items-center justify-center gap-3">
-				<Toggle
-					size="28"
-					variant="ghost"
-					color="primary"
-					className="group/toggle">
+				<Toggle size="28" variant="ghost" color="primary">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Primary
 				</Toggle>
-				<Toggle
-					size="28"
-					variant="outline"
-					color="primary"
-					className="group/toggle">
+				<Toggle size="28" variant="outline" color="primary">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Primary
 				</Toggle>
 			</div>
 
 			<div className="flex items-center justify-center gap-3">
-				<Toggle size="28" variant="ghost" color="info" className="group/toggle">
+				<Toggle size="28" variant="ghost" color="info">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Info
 				</Toggle>
-				<Toggle
-					size="28"
-					variant="outline"
-					color="info"
-					className="group/toggle">
+				<Toggle size="28" variant="outline" color="info">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Info
 				</Toggle>
 			</div>
 
 			<div className="flex items-center justify-center gap-3">
-				<Toggle
-					size="28"
-					variant="ghost"
-					color="success"
-					className="group/toggle">
+				<Toggle size="28" variant="ghost" color="success">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Success
 				</Toggle>
-				<Toggle
-					size="28"
-					variant="outline"
-					color="success"
-					className="group/toggle">
+				<Toggle size="28" variant="outline" color="success">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Success
 				</Toggle>
 			</div>
 
 			<div className="flex items-center justify-center gap-3">
-				<Toggle
-					size="28"
-					variant="ghost"
-					color="error"
-					className="group/toggle">
+				<Toggle size="28" variant="ghost" color="error">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Error
 				</Toggle>
-				<Toggle
-					size="28"
-					variant="outline"
-					color="error"
-					className="group/toggle">
+				<Toggle size="28" variant="outline" color="error">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Error
 				</Toggle>
 			</div>
 
 			<div className="flex items-center justify-center gap-3">
-				<Toggle
-					size="28"
-					variant="ghost"
-					color="warning"
-					className="group/toggle">
+				<Toggle size="28" variant="ghost" color="warning">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Warning
 				</Toggle>
-				<Toggle
-					size="28"
-					variant="outline"
-					color="warning"
-					className="group/toggle">
+				<Toggle size="28" variant="outline" color="warning">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Warning
 				</Toggle>
 			</div>
 
 			<div className="flex items-center justify-center gap-3">
-				<Toggle
-					size="28"
-					variant="ghost"
-					color="neutral"
-					className="group/toggle">
+				<Toggle size="28" variant="ghost" color="neutral">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Neutral
 				</Toggle>
-				<Toggle
-					size="28"
-					variant="outline"
-					color="neutral"
-					className="group/toggle">
+				<Toggle size="28" variant="outline" color="neutral">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Neutral
 				</Toggle>
--- apps/website/src/registry/example/toggle/toggle-sizes.tsx
+++ apps/website/src/registry/example/toggle/toggle-sizes.tsx
@@ -6,66 +6,42 @@ const ToggleSizesExample = () => {
 	return (
 		<div className="flex flex-col items-center justify-center gap-5">
 			<div className="flex items-center justify-center gap-3">
-				<Toggle
-					variant="outline"
-					color="neutral"
-					size="28"
-					className="group/toggle">
+				<Toggle variant="outline" color="neutral" size="28">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Size 28
 				</Toggle>
 			</div>
 
 			<div className="flex items-center justify-center gap-3">
-				<Toggle
-					variant="outline"
-					color="neutral"
-					size="32"
-					className="group/toggle">
+				<Toggle variant="outline" color="neutral" size="32">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Size 32
 				</Toggle>
 			</div>
 
 			<div className="flex items-center justify-center gap-3">
-				<Toggle
-					variant="outline"
-					color="neutral"
-					size="36"
-					className="group/toggle">
+				<Toggle variant="outline" color="neutral" size="36">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Size 36
 				</Toggle>
 			</div>
 
 			<div className="flex items-center justify-center gap-3">
-				<Toggle
-					variant="outline"
-					color="neutral"
-					size="40"
-					className="group/toggle">
+				<Toggle variant="outline" color="neutral" size="40">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Size 40
 				</Toggle>
 			</div>
 
 			<div className="flex items-center justify-center gap-3">
-				<Toggle
-					variant="outline"
-					color="neutral"
-					size="44"
-					className="group/toggle">
+				<Toggle variant="outline" color="neutral" size="44">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Size 44
 				</Toggle>
 			</div>
 
 			<div className="flex items-center justify-center gap-3">
-				<Toggle
-					variant="outline"
-					color="neutral"
-					size="48"
-					className="group/toggle">
+				<Toggle variant="outline" color="neutral" size="48">
 					<BookmarkIcon className="group-data-[state=on]/toggle:fill-current" />
 					Size 48
 				</Toggle>
