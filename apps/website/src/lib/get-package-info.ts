f47698cd7487badd1be4db3cc708fbdc113ccac7
Bijay Khapung <khapungbj84@gmail.com>
2026-06-03 13:43:26 +0545

    Revert "fix: add error handling to get-package-info fetch call"
    This reverts commit 9686f0ed23a430fb9cf647bf475dca3537c90a3a.

--- apps/website/src/lib/get-package-info.ts
+++ apps/website/src/lib/get-package-info.ts
@@ -1,13 +1,8 @@
 export const getPackageVersion = async () => {
-	try {
-		const version = await fetch("https://registry.npmjs.org/radianui", {
-			cache: "no-store",
-		})
-			.then((res) => res.json())
-			.then((data) => data["dist-tags"].latest)
-		return version
-	} catch (err) {
-		console.error(err)
-		return 0
-	}
+	const version = await fetch("https://registry.npmjs.org/radianui", {
+		cache: "no-store",
+	})
+		.then((res) => res.json())
+		.then((data) => data["dist-tags"].latest)
+	return version
 }
