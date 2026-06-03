9686f0ed23a430fb9cf647bf475dca3537c90a3a
Bijay Khapung <khapungbj84@gmail.com>
2026-06-03 13:25:56 +0545

    fix: add error handling to get-package-info fetch call

--- apps/website/src/lib/get-package-info.ts
+++ apps/website/src/lib/get-package-info.ts
@@ -1,8 +1,13 @@
 export const getPackageVersion = async () => {
-	const version = await fetch("https://registry.npmjs.org/radianui", {
-		cache: "no-store",
-	})
-		.then((res) => res.json())
-		.then((data) => data["dist-tags"].latest)
-	return version
+	try {
+		const version = await fetch("https://registry.npmjs.org/radianui", {
+			cache: "no-store",
+		})
+			.then((res) => res.json())
+			.then((data) => data["dist-tags"].latest)
+		return version
+	} catch (err) {
+		console.error(err)
+		return 0
+	}
 }
