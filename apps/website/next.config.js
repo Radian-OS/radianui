c90ad74adcb29a64a965536ab1ebf2cae77128d5
Bijay Khapung <khapungbj84@gmail.com>
2026-06-04 14:12:47 +0545

    chore: increase cache ttl for built static js, css files

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -10,6 +10,15 @@ const nextConfig = {
 	async headers() {
 		return [
 			{
+				source: "/_next/static/:path*",
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "public, max-age=31536000, immutable",
+					},
+				],
+			},
+			{
 				source: "/:path*.(svg|ico|png|jpg|jpeg|webp|woff|woff2)",
 				headers: [
 					{
