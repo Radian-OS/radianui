da39010642303b1331282809423348fffa6df4c4
Ashmit72 <ashmitbastola@gmail.com>
2026-06-26 12:35:41 +0545

    chore: fix dropdown example with svg

--- apps/website/src/registry/ui/stepper.tsx
+++ apps/website/src/registry/ui/stepper.tsx
@@ -31,6 +31,8 @@ import { cn } from "@/lib/utils"
 
 /* eslint-disable react-hooks/exhaustive-deps */
 
+/* eslint-disable react-hooks/exhaustive-deps */
+
 // Types
 type StepperOrientation = "horizontal" | "vertical"
 type StepState = "active" | "completed" | "inactive" | "loading"
