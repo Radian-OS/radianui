9a8316fa3cd67a741b47b273b83d6f3397ce6171
Bijay Khapung <khapungbj84@gmail.com>
2026-07-10 14:08:02 +0545

    style: update changelog

--- apps/website/src/components/changelog-list.tsx
+++ apps/website/src/components/changelog-list.tsx
@@ -1,5 +1,7 @@
 import { allChangelogs } from "contentlayer/generated"
+import { format } from "date-fns"
 import { Mdx } from "@/components/mdx"
+import { Badge } from "@/registry/ui/badge"
 
 export default function ChangelogList() {
 	const sortedChangelogs = allChangelogs.sort(
@@ -8,9 +10,18 @@ export default function ChangelogList() {
 
 	return (
 		<div className="my-8 flex flex-col gap-16">
-			{sortedChangelogs.map((changelog) => (
-				<Mdx key={changelog._id} code={changelog.body.code} />
-			))}
+			{sortedChangelogs.map((changelog) => {
+				const formattedDate = format(new Date(changelog.date), "d MMMM, yyyy")
+
+				return (
+					<div key={changelog._id} className="flex flex-col items-start">
+						<Badge variant="soft" color="primary" size="24">
+							{formattedDate}
+						</Badge>
+						<Mdx code={changelog.body.code} />
+					</div>
+				)
+			})}
 		</div>
 	)
 }
--- apps/website/src/components/feature-list.tsx
+++ apps/website/src/components/feature-list.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 export function FeatureList({ children }: { children: React.ReactNode }) {
-	return <ul className="m-0 list-none space-y-2.5 p-0">{children}</ul>
+	return <ul className="m-0 mt-3 list-none space-y-2.5 p-0">{children}</ul>
 }
 
 export function FeatureListItem({
@@ -14,7 +14,7 @@ export function FeatureListItem({
 	rightElement?: React.ReactNode
 }) {
 	return (
-		<li className="bg-fill1 border-primary-border text-fg-secondary flex items-center gap-2 border-l-4 px-4 py-2 text-lg font-medium">
+		<li className="bg-fill1 border-primary-border text-fg-secondary flex items-center gap-2 border-l-4 px-5 py-2 text-sm">
 			{icon && <span className="shrink-0 text-lg">{icon}</span>}
 			<span className="flex-1">{children}</span>
 			{rightElement && <span>{rightElement}</span>}
