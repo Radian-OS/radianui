e3809f9080965427a9ea7699fb95ad1aa30e0b1e
yube01 <yubraja008@gmail.com>
2026-07-28 16:13:51 +0545

    fix: line height, font weight and text size update and border beam removed from flexiblity section

--- apps/website/src/app/home/component.tsx
+++ apps/website/src/app/home/component.tsx
@@ -42,76 +42,80 @@ const Component = () => {
 	}, [api])
 
 	return (
-		<section className="border-soft z-10 mx-auto flex max-w-[1440px] flex-col border">
-			<div className="md:px-15 md:py-30 flex flex-col gap-8 px-5 py-16 sm:px-10">
-				<div className="flex flex-col gap-4 sm:gap-6">
-					<Badge color="primary" size="28" variant="soft">
-						<ComponentIcon className="text-primary size-4" />
-						Components
-					</Badge>
-					<div className="w-full md:w-[940px]">
-						<h2 className="heading-4">
-							<span className="text-[32px]">
-								Production-ready React and Figma UI components.
-							</span>{" "}
-							<span className="text-fg-secondary text-[32px]">
-								Build modern interfaces faster with customizable React
-								components and Figma UI components.
-							</span>
-						</h2>
+		<section
+			aria-labelledby="components-title"
+			className="max-w-368 relative z-20 mx-auto w-full px-4 lg:px-5">
+			<div className="border-soft z-10 mx-auto flex max-w-[1440px] flex-col border border-t-0">
+				<div className="md:px-15 md:py-30 flex flex-col gap-8 px-5 py-16 sm:px-10">
+					<div className="flex flex-col gap-4 sm:gap-6">
+						<Badge color="primary" size="28" variant="soft">
+							<ComponentIcon className="text-primary size-4" />
+							Components
+						</Badge>
+						<div className="w-full md:w-[940px]">
+							<h2 className="heading-4">
+								<span className="text-[32px] font-medium leading-[44px] tracking-[-0.32px]">
+									Production-ready React and Figma UI components.
+								</span>{" "}
+								<span className="text-fg-secondary text-[32px] font-medium leading-[44px] tracking-[-0.32px]">
+									Build modern interfaces faster with customizable React
+									components and Figma UI components.
+								</span>
+							</h2>
+						</div>
 					</div>
-				</div>
 
-				<div className="flex justify-between">
-					<Button>View all Components</Button>
-					<div className="flex gap-2.5">
-						<IconButton
-							aria-label="ChevronLeft"
-							color="neutral"
-							size="32"
-							variant="outline"
-							onClick={handlePrev}
-							disabled={!canScrollPrev}>
-							<ChevronLeft className="text-fg-secondary" />
-						</IconButton>
-						<IconButton
-							aria-label="ChevronRight"
-							color="neutral"
-							size="32"
-							variant="outline"
-							onClick={handleNext}
-							disabled={!canScrollNext}>
-							<ChevronRight className="text-fg-secondary" />
-						</IconButton>
+					<div className="flex justify-between">
+						<Button>View all Components</Button>
+						<div className="flex gap-2.5">
+							<IconButton
+								aria-label="ChevronLeft"
+								color="neutral"
+								size="32"
+								variant="outline"
+								onClick={handlePrev}
+								disabled={!canScrollPrev}>
+								<ChevronLeft className="text-fg-secondary" />
+							</IconButton>
+							<IconButton
+								aria-label="ChevronRight"
+								color="neutral"
+								size="32"
+								variant="outline"
+								onClick={handleNext}
+								disabled={!canScrollNext}>
+								<ChevronRight className="text-fg-secondary" />
+							</IconButton>
+						</div>
 					</div>
 				</div>
+				<div className="border-soft relative z-10 w-full border-y px-5">
+					<Carousel
+						setApi={onApiChange}
+						opts={{
+							align: "start",
+							slidesToScroll: 1,
+						}}>
+						<CarouselContent>
+							{HOMEPAGE_COMPONENTS_LIST.map((item, idx) => (
+								<CarouselItem
+									key={item.title + idx}
+									className="basis-full sm:basis-1/2 lg:basis-1/4">
+									<ComponentCard
+										alt={item.alt!}
+										url={item.url}
+										title={item.title}
+										description={item.description!}
+										thumbnail={item.thumbnail!}
+										thumbnailDark={item.thumbnailDark!}
+									/>
+								</CarouselItem>
+							))}
+						</CarouselContent>
+					</Carousel>
+				</div>
+				<DiagonalDivider className="w-full" />
 			</div>
-			<div className="border-soft relative z-10 w-full border-y px-5">
-				<Carousel
-					setApi={onApiChange}
-					opts={{
-						align: "start",
-						slidesToScroll: 1,
-					}}>
-					<CarouselContent>
-						{HOMEPAGE_COMPONENTS_LIST.map((item, idx) => (
-							<CarouselItem
-								key={item.title + idx}
-								className="basis-full sm:basis-1/2 lg:basis-1/4">
-								<ComponentCard
-									alt={item.alt!}
-									url={item.url}
-									title={item.title}
-									description={item.description!}
-									thumbnail={item.thumbnail!}
-									thumbnailDark={item.thumbnailDark!}
-								/>
-							</CarouselItem>
-						))}
-					</CarouselContent>
-				</Carousel>
-			</div>
-			<DiagonalDivider className="w-full" />
 		</section>
 	)
 }
