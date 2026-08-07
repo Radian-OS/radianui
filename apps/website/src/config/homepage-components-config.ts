135ca72992a1e272e5dbb0ba0919e0cedff79c30
yube01 <yubraja008@gmail.com>
2026-08-07 10:01:47 +0545

    fix: component section card color update

--- apps/website/src/config/homepage-components-config.ts
+++ apps/website/src/config/homepage-components-config.ts
@@ -6,6 +6,7 @@ import {
 	ButtonThumbnail,
 	CarouselThumbnail,
 	CheckboxThumbnail,
+	DialogThumbnail,
 	DropdownThumbnail,
 	FileUploadThumbnail,
 	InputThumbnail,
@@ -126,6 +127,13 @@ export const HOMEPAGE_COMPONENTS_LIST: {
 		url: "/docs/components/tooltip",
 		thumbnail: TooltipThumbnail,
 		alt: "Tooltip UI component illustration",
+	},
+	{
+		title: "Dialog",
+		description: "4 Components, 92 Variants",
+		url: "/docs/components/dialog",
+		thumbnail: DialogThumbnail,
+		alt: "Dialog UI component illustration",
 	},
 	{
 		title: "Carousel",
